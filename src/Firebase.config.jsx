1c09b2398a1e56799ab366437769b92dd5a7131d
ujjal zaman <49386888+Ujjalzaman@users.noreply.github.com>
2021-08-09 00:33:18 +0600

    Update Firebase.config.jsx

--- src/Firebase.config.jsx
+++ src/Firebase.config.jsx
@@ -1,8 +1,1 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBFG21Ae_dyH9yVplCbvxakxwGlgWuneIo",
-    authDomain: "online-doctor-appointmen-e3d9b.firebaseapp.com",
-    projectId: "online-doctor-appointmen-e3d9b",
-    storageBucket: "online-doctor-appointmen-e3d9b.appspot.com",
-    messagingSenderId: "686148012062",
-    appId: "1:686148012062:web:ec3dab9f245798c1e5060f"
-}; export default firebaseConfig;
+;
