8f8524aa8d200aefcf5d8b37c5ddf98450044dd0
ujjalz <ujjalz@thegates.biz>
2022-12-06 10:13:53 +0600

    Cleanup code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthContextProvider } from './components/Context/AuthContext';
 
@@ -15,5 +13,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
-reportWebVitals();
