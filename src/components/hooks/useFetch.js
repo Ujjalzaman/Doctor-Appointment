f9c0b2f38b48b788df9b4a47a30e3c7d79feaa56
ujjalz <ujjalz@thegates.biz>
2022-12-30 21:12:04 +0600

    Implemented Custom Hooks for fetch API

---
+++ src/components/hooks/useFetch.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useFetch = (url) =>{
+    const [data, setData] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() =>{
+        const fetchData = async() =>{
+            setLoading(true);
+            try{
+                const res = await axios.get(url);
+                setData(res.data);
+            }catch(err){
+                setError(err);
+                setLoading(false)
+            }
+            setLoading(false);
+        }
+        fetchData();
+    },[url])
+    const reFetchData = async() =>{
+        setLoading(true);
+        try{
+            const res = await axios.get(url);
+            setData(res.data);
+        }
+        catch(err){
+            setError(err);
+            setLoading(false);
+        }
+        setLoading(false);
+    }
+
+    return {data, loading, error, reFetchData}
+}
+
+export default useFetch;
