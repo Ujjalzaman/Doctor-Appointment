5fb183415fab5ee59702654b3befdb66863180d7
ujjalz <ujjalz@thegates.biz>
2023-01-06 22:21:33 +0600

    Fixed Appointment short list

--- src/components/AppointMent/AppointMentShortList/AppointMentShorList.js
+++ src/components/AppointMent/AppointMentShortList/AppointMentShorList.js
@@ -1,24 +1,23 @@
 import React from 'react';
-
+import Table from 'react-bootstrap/Table';
 const AppointMentShorList = ({ appointment }) => {
     return (
-        <div className="container">
-            <table className="table table-borderless">
-                <thead className="text-center">
-                    <tr>
-                        <th className="text-secondary" scope="col">Name</th>
-                        <th className="text-secondary" scope="col">Phone</th>
-                        <th className="text-secondary" scope="col">Email</th>
-                        <th className="text-secondary" scope="col">Gender</th>
-                        <th className="text-secondary" scope="col">Service</th>
-                        <th className="text-secondary" scope="col">Date</th>
-                        <th className="text-secondary" scope="col">Weight</th>
-                        <th className="text-secondary" scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody className="text-center">
-                    {appointment && appointment?.map((item, index) =>
-                    <tr key={index+200}>
+        <Table striped responsive>
+            <thead>
+                <tr>
+                    <th className="text-secondary" scope="col">Name</th>
+                    <th className="text-secondary" scope="col">Phone</th>
+                    <th className="text-secondary" scope="col">Email</th>
+                    <th className="text-secondary" scope="col">Gender</th>
+                    <th className="text-secondary" scope="col">Service</th>
+                    <th className="text-secondary" scope="col">Date</th>
+                    <th className="text-secondary" scope="col">Weight</th>
+                    <th className="text-secondary" scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {appointment && appointment?.map((item, index) =>
+                    <tr key={index + 200}>
                         <td>{item.name}</td>
                         <td>{item.phone}</td>
                         <td>{item.email}</td>
@@ -30,13 +29,9 @@ const AppointMentShorList = ({ appointment }) => {
                             <button className="btn btn-primary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Not Visited</button>
                         </td>
                     </tr>
-
-
-
-                    )} 
-                </tbody>
-            </table>
-        </div>
+                )}
+            </tbody>
+        </Table>
     );
 };
 
--- src/components/Dashboard/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard/Dashboard.js
@@ -11,18 +11,18 @@ const Dashboard = () => {
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [appointByDate, setAppointments] = useState([]);
     const { data, loading, error } = useFetch("http://localhost:5000/auth/patients");
-    if(appointByDate.length == 0){
+    if (appointByDate.length == 0) {
         setAppointments(data);
     }
     const handleDateChange = date => {
         setSelectedDate(date);
     }
-    useEffect(() =>{
-        const fetchData = async () =>{
-            try{
-                const res = await axios.post("http://localhost:5000/auth/appointByDate", {date:selectedDate})
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.post("http://localhost:5000/auth/appointByDate", { date: selectedDate })
                 setAppointments(res.data)
-            }catch(err){
+            } catch (err) {
                 console.log(err)
             }
         }
@@ -31,25 +31,24 @@ const Dashboard = () => {
     }, [selectedDate])
 
     return (
-        <section className="dashboard">
-            <div className="desh-sidebar">
+        <section className='container row g-0'>
+            <div className='col-md-3'>
                 <Sidebar></Sidebar>
             </div>
-            <div className="container dash-main">
-                <div className="row justify-content-center g-0">
-                    <div className="desh-calender col-md-4 col-sm-12 col-12 m-0 p-0">
+            <div className="col-md-9">
+                <div className=''>
+                    <div className='mb-2'>
                         <Calendar
                             onChange={handleDateChange}
                             value={new Date()}
                             className="calender-design"
                         />
                     </div>
-                    <div className="col-md-8 col-sm-12 col-12 desh-appointment m-0 p-0">
+                    <div>
                         <AppointByDate appopintMent={appointByDate} key={10} selectedDate={selectedDate}></AppointByDate>
                     </div>
                 </div>
             </div>
-
         </section>
     );
 };
