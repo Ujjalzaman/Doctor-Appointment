568342b5a38b09f99faddf91f37a30e9f8762596
ujjalzaman <muzzwal97@gmail.com>
2023-07-23 19:44:43 +0600

    Implemented Patients Apis and All the related Functionalities

---
+++ api/src/app/modules/patient/patient.controller.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import { IPatient } from "./patient.interface";
+import { PatientService } from "./patient.service";
+
+const createPatient = catchAsync(async (req: Request, res: Response) => {
+    const { ...patientInfo } = req.body;
+    const result = await PatientService.createPatient(patientInfo);
+    sendResponse<IPatient>(res, {
+        statusCode: 200,
+        message: 'Successfully patient created !!',
+        success: true,
+        data: result,
+    })
+})
+
+const getAllPatient = catchAsync(async (req: Request, res: Response) => {
+    const result = await PatientService.getAllPatient();
+    sendResponse<IPatient[]>(res, {
+        statusCode: 200,
+        message: 'Successfully Retriev All Patient !!',
+        success: true,
+        data: result,
+    })
+})
+
+const getSinglePatient = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await PatientService.getSinglePatient(id);
+    sendResponse<IPatient>(res, {
+        statusCode: 200,
+        message: 'Successfully Get Single Patient !!',
+        success: true,
+        data: result,
+    })
+})
+
+const deletePatient = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    await PatientService.deletePatient(id);
+    sendResponse<IPatient>(res, {
+        statusCode: 200,
+        message: 'Successfully Deleted Patient !!',
+        success: true,
+    })
+})
+
+const updatePatient = catchAsync(async (req: Request, res: Response) => {
+    const { ...patientInfo } = req.body;
+    const { id } = req.params;
+    const result = await PatientService.updatePatient(id, patientInfo);
+    sendResponse<IPatient>(res, {
+        statusCode: 200,
+        message: 'Successfully Updated Patient informations !!',
+        success: true,
+        data: result,
+    })
+})
+
+export const PatientController = {
+    updatePatient,
+    deletePatient,
+    getAllPatient,
+    getSinglePatient,
+    createPatient
+}
---
+++ api/src/app/modules/patient/patient.service.ts
@@ -0,0 +1,34 @@
+import { IPatient } from "./patient.interface";
+import { PatientModel } from "./patient.model";
+
+const createPatient = async (payload: IPatient): Promise<IPatient | null> => {
+    const result = await PatientModel.create(payload);
+    return result;
+}
+
+const getAllPatient = async (): Promise<IPatient[] | null> => {
+    const result = await PatientModel.find();
+    return result;
+}
+
+const getSinglePatient = async (patientId: string): Promise<IPatient | null> => {
+    const result = await PatientModel.findOne({ _id: patientId });
+    return result;
+}
+
+const deletePatient = async (patientId: string): Promise<void> => {
+    await PatientModel.findOneAndDelete({ _id: patientId });
+}
+
+const updatePatient = async (patientId: string, payload: Partial<IPatient>): Promise<IPatient | null> => {
+    const result = await PatientModel.findOneAndUpdate({ _id: patientId }, { payload });
+    return result;
+}
+
+export const PatientService = {
+    updatePatient,
+    getAllPatient,
+    getSinglePatient,
+    deletePatient,
+    createPatient
+}
