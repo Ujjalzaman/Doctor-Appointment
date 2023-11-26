3907c096949d0162c6ce09a8e5d2bba9e64de13c
ujjalzaman <muzzwal97@gmail.com>
2023-11-27 01:50:01 +0600

    Updated Navbar and footer UI

--- src/components/AppointMent/AppointMent/AppointMent.js
+++ src/components/AppointMent/AppointMent/AppointMent.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Navbar from '../../Shared/Navbar/Navbar';
-import AppointMentForm from '../AppointMentForm/AppointMentForm';
 import AppointMentTop from '../AppointMentTop/AppointMentTop';
 import BookApointMent from '../BookApointMent/BookApointMent';
 // import 
@@ -15,7 +14,6 @@ const AppointMent = () => {
             <Navbar></Navbar>
             <AppointMentTop handleDateChange={handleDateChange} key={10}></AppointMentTop>
             <BookApointMent selectDate={selectDate} key={6}></BookApointMent>
-            {/* <AppointMentForm></AppointMentForm> */}
             <Footer></Footer>
         </div>
     );
--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,64 +1,105 @@
 import React from 'react';
-import FooterDetail from './FooterDetail';
 import './Footer.css';
+import logo from '../../../images/logo.png';
 
 const Footer = () => {
-    const noNamed = [
-        { name: "Emergency Dental Care", link: "/emergency" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Treatment of Personal Diseases", link: "/personal-treatment" },
-        { name: "Tooth Extraction", link: "/tooth-extract" },
-        { name: "Check Up", link: "/checkup" },
-    ]
-    const ourAddress = [
-        { name: "New York - 101010 Hudson", link: "//google.com/map" },
-        { name: "Yards", link: "//google.com/map" },
+	return (
+		<footer className="footer">
 
-    ]
-    const oralHealth = [
-        { name: "Emergency Dental Care", link: "/emergency" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Treatment of Personal Diseases", link: "/personal-treatment" },
-        { name: "Tooth Extraction", link: "/tooth-extract" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Check Up", link: "/checkup" }
-    ]
-    const services = [
-        { name: "Emergency Dental Care", link: "/emergency" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Treatment of Personal Diseases", link: "/personal-treatment" },
-        { name: "Tooth Extraction", link: "/tooth-extract" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Check Up", link: "/checkup" }
-    ]
 
-    return (
-        <footer className="footer-area clear-both sm-pe-5">
-            <div className="container pt-5">
-                <div className="row md-py-5 footer-content">
-                    {/* <FooterDetail key={1} menuTitle={"."} menuItems={noNamed} /> */}
-                    <FooterDetail key={2} menuTitle="Services" menuItems={services} />
-                    <FooterDetail key={3} menuTitle="Oral Health" menuItems={oralHealth} />
-                    <FooterDetail key={4} menuTitle="Our Address" menuItems={ourAddress}>
-                        <ul className="social-media list-inline">
-                            <li className="list-inline-item"><a href="!#"></a></li>
-                            <li className="list-inline-item"><a href="!#"></a></li>
-                            <li className="list-inline-item"><a href="!#"></a></li>
-                        </ul>
-                        <div className="md-mt-5">
-                            <h6>Call now</h6>
-                            <button className="btn btn-brand">+2025550295</button>
-                        </div>
-                    </FooterDetail>
-                </div>
-                <div className="copyRight text-center">
-                    <p>Copyright {(new Date()).getFullYear()} All Rights Reserved</p>
-                </div>
-            </div>
-        </footer>
-    );
+			<div className="footer-top">
+				<div className="container-fluid">
+					<div className="row">
+						<div className="col-lg-3 col-md-6">
+							<div className="footer-widget footer-about">
+								<div className="footer-logo">
+									<img src={logo} alt="logo" style={{maxWidth:'160px'}}/>
+								</div>
+								<div className="footer-about-content">
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+								</div>
+							</div>
+
+						</div>
+
+						<div className="col-lg-3 col-md-6">
+							<div className="footer-widget footer-menu">
+								<h2 className="footer-title">For Patients</h2>
+								<ul>
+									<li><a href="#"><i className="fas fa-angle-double-right"></i> Search for Doctors</a></li>
+									<li><a href="#"><i className="fas fa-angle-double-right"></i> Login</a></li>
+									<li><a href="#"><i className="fas fa-angle-double-right"></i> Register</a></li>
+									<li><a href="#"><i className="fas fa-angle-double-right"></i> Booking</a></li>
+									<li><a href="#"><i className="fas fa-angle-double-right"></i> Patient Dashboard</a></li>
+								</ul>
+							</div>
+						</div>
+
+						<div className="col-lg-3 col-md-6">
+
+							<div className="footer-widget footer-menu">
+								<h2 className="footer-title">For Doctors</h2>
+								<ul>
+									<li><a href="#"><i className="fas fa-angle-double-right"></i> Appointments</a></li>
+									<li><a href="#"><i className="fas fa-angle-double-right"></i> Chat</a></li>
+									<li><a href="#"><i className="fas fa-angle-double-right"></i> Login</a></li>
+									<li><a href="#"><i className="fas fa-angle-double-right"></i> Register</a></li>
+									<li><a href="#"><i className="fas fa-angle-double-right"></i> Doctor Dashboard</a></li>
+								</ul>
+							</div>
+						</div>
+
+						<div className="col-lg-3 col-md-6">
+							<div className="footer-widget footer-contact">
+								<h2 className="footer-title">Contact Us</h2>
+								<div className="footer-contact-info">
+									<div className="footer-address">
+										<span><i className="fas fa-map-marker-alt"></i></span>
+										<p> 3556  Beech Street, San Francisco,<br /> California, CA 94108 </p>
+									</div>
+									<p>
+										<i className="fas fa-phone-alt"></i>
+										+1 315 369 5943
+									</p>
+									<p className="mb-0">
+										<i className="fas fa-envelope"></i>
+										ujjalzaman@gmail.com
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="footer-bottom">
+				<div className="container-fluid">
+
+					<div className="copyright">
+						<div className="row">
+							<div className="col-md-6 col-lg-6">
+								<div className="copyright-text">
+									<p className="mb-0"><a href="templateshub.net">
+										<div className="copyRight text-center">
+											<p>Copyright {(new Date()).getFullYear()} All Rights Reserved</p>
+										</div></a></p>
+								</div>
+							</div>
+							<div className="col-md-6 col-lg-6">
+								<div className="copyright-menu">
+									<ul className="policy-menu d-flex gap-2">
+										<a href="#" className='text-white'>Terms and Conditions</a>
+										<a href="#" className='text-white'>Policy</a>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+		</footer>
+	);
 };
 
 export default Footer;
