dac139860de373f96fb7cfb3788f95efc837dd3b
ujjalzaman <muzzwal97@gmail.com>
2023-11-28 17:57:54 +0600

    Implemented Dashboard layout and Ui structer

---
+++ src/components/Admin/Profile/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AdminLayout from '../AdminLayout/AdminLayout'
+import userImg from '../../../images/john.png';
+import './Profile.css';
+
+const Profile = () => {
+    return (
+        <>
+            <AdminLayout >
+               
+            </AdminLayout>
+        </>
+    )
+}
+export default Profile;
