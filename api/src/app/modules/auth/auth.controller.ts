843e764a22727ab32bb129df3d7e264f3d8ff08f
ujjalzaman <muzzwal97@gmail.com>
2023-12-01 23:58:34 +0600

    Intregated with relational database Prisma and followed module pattern

--- api/src/app/modules/auth/auth.controller.ts
+++ api/src/app/modules/auth/auth.controller.ts
@@ -1,92 +1,19 @@
 import { Request, Response } from "express";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
-import { AuthService } from "./auth.service";
 import { IAuth } from "./auth.interface";
+import { AuthService } from "./auth.service";
 
-const createPatient = catchAsync(async (req: Request, res: Response) => {
-    const { patient, ...otherInfo } = req.body;
-    const result = await AuthService.createPatient(patient, otherInfo);
-
+const SignUP = catchAsync(async (req: Request, res: Response) => {
+    const result = await AuthService.AuthSignup(req.body);
     sendResponse<IAuth>(res, {
         statusCode: 200,
-        message: 'Successfully Patient created !!',
-        success: true,
-        data: result,
-    })
-})
-const createDoctor = catchAsync(async (req: Request, res: Response) => {
-    const { doctor, ...otherInfo } = req.body;
-    const result = await AuthService.createPatient(doctor, otherInfo);
-
-    sendResponse<IAuth>(res, {
-        statusCode: 200,
-        message: 'Successfully Doctor created !!',
-        success: true,
-        data: result,
-    })
-})
-const createAdmin = catchAsync(async (req: Request, res: Response) => {
-    const { admin, ...otherInfo } = req.body;
-    const result = await AuthService.createPatient(admin, otherInfo);
-
-    sendResponse<IAuth>(res, {
-        statusCode: 200,
-        message: 'Successfully Admin created !!',
-        success: true,
-        data: result,
-    })
-})
-
-const getAllAuth = catchAsync(async (req: Request, res: Response) => {
-    const result = await AuthService.getAllAuth();
-    sendResponse<IAuth[]>(res, {
-        statusCode: 200,
-        message: 'Successfully Retriev All Auth !!',
-        success: true,
-        data: result,
-    })
-})
-
-const getSingleAuth = catchAsync(async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const result = await AuthService.getSingleAuth(id);
-    sendResponse<IAuth>(res, {
-        statusCode: 200,
-        message: 'Successfully Get Single Auth !!',
-        success: true,
-        data: result,
-    })
-})
-
-const deleteAuth = catchAsync(async (req: Request, res: Response) => {
-    const { id } = req.params;
-    await AuthService.deleteAuth(id);
-    sendResponse<IAuth>(res, {
-        statusCode: 200,
-        message: 'Successfully Deleted Auth !!',
-        success: true,
-    })
-})
-
-const updateAuth = catchAsync(async (req: Request, res: Response) => {
-    const { ...appointInfo } = req.body;
-    const { id } = req.params;
-    const result = await AuthService.updateAuth(id, appointInfo);
-    sendResponse<IAuth>(res, {
-        statusCode: 200,
-        message: 'Successfully Updated Auth informations !!',
+        message: 'Successfully Signup !!',
         success: true,
         data: result,
     })
 })
 
 export const AuthController = {
-    updateAuth,
-    deleteAuth,
-    getAllAuth,
-    getSingleAuth,
-    createPatient,
-    createDoctor,
-    createAdmin
+    SignUP
 }
