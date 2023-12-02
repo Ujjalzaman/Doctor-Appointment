0db4183f1ca589d7175d051afa5743b7060408d6
ujjalzaman <muzzwal97@gmail.com>
2023-12-02 18:11:16 +0600

    Implemented doctor api

---
+++ api/src/app/modules/doctor/doctorService.ts
@@ -0,0 +1,34 @@
+import httpStatus from "http-status";
+import ApiError from "../../../errors/apiError";
+import prisma from "../../../shared/prisma";
+import { UserRole } from "@prisma/client";
+import bcrypt from 'bcrypt';
+
+export const create = async (payload: any): Promise<any> => {
+    try {
+        const data = await prisma.$transaction(async (tx) => {
+            const { password, ...othersData } = payload;
+            const patient = await tx.doctor.create({
+                data: othersData,
+            });
+
+            if (patient) {
+                const auth = await tx.auth.create({
+                    data: {
+                        email: patient.email,
+                        password: password && await bcrypt.hashSync(password, 12),
+                        role: UserRole.doctor,
+                    },
+                });
+                return {
+                    patient,
+                    auth,
+                };
+            }
+        });
+
+        return data;
+    } catch (error:any) {
+        throw new ApiError(httpStatus.BAD_REQUEST, error.message)
+    }
+};
