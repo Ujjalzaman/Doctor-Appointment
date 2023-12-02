ae6e53f47769823a010febef31c62346c62d7f83
ujjalzaman <muzzwal97@gmail.com>
2023-12-02 18:50:46 +0600

    Implemented authentication

--- api/src/app/modules/auth/auth.service.ts
+++ api/src/app/modules/auth/auth.service.ts
@@ -1,31 +1,39 @@
-import {Auth} from '@prisma/client';
 import bcrypt from 'bcrypt';
 import prisma from "../../../shared/prisma";
+import ApiError from '../../../errors/apiError';
+import httpStatus from 'http-status';
+import { JwtHelper } from '../../../helpers/jwtHelper';
+import config from '../../../config';
+import { Secret } from 'jsonwebtoken';
 
-// const AuthSignup = async(data:Auth):Promise<Auth | null> =>{
-//     let user: Auth = data
-//     const {password} = user;
-//     if(password){
-//         user.password = await bcrypt.hashSync(password, 12)
-//     }
-//     const result = await prisma.auth.create({
-//         data: user
-//     })
-//     return result
-// }
+type ILginResponse = {
+    accessToken?: string;
+    user: {}
+}
 
-const loginUser = async(data:Auth):Promise<Auth | null> =>{
-    let user: Auth = data
-    const {password} = user;
-    if(password){
-        user.password = await bcrypt.hashSync(password, 12)
-    }
-    const result = await prisma.auth.create({
-        data: user
+const loginUser = async (user: any): Promise<ILginResponse> => {
+    const { email: IEmail, password } = user;
+    const isUserExist = await prisma.auth.findUnique({
+        where: { email: IEmail }
     })
-    return result
+
+    if (!isUserExist) {
+        throw new ApiError(httpStatus.NOT_FOUND, "User is not Exist !");
+    }
+    const isPasswordMatched = await bcrypt.compare(password, isUserExist.password);
+
+    if (!isPasswordMatched) {
+        throw new ApiError(httpStatus.NOT_FOUND, "Password is not Matched !");
+    }
+    const { email, id, role } = isUserExist;
+    const accessToken = JwtHelper.createToken(
+        { email, id, role },
+        config.jwt.secret as Secret,
+        config.jwt.JWT_EXPIRES_IN as string
+    )
+    return { accessToken, user: { email, role, id } }
 }
 
 export const AuthService = {
-    // AuthSignup
+    loginUser
 }
