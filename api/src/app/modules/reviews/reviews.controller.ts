23326e5e88c94180912eb32e2720516eee695f5a
ujjalzaman <muzzwal97@gmail.com>
2023-12-03 13:03:24 +0600

    Implmented reviews api

---
+++ api/src/app/modules/reviews/reviews.controller.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import { Reviews } from "@prisma/client";
+import { ReviewService } from "./reviews.service";
+
+const creatReview = catchAsync(async (req: Request, res: Response) => {
+    const result = await ReviewService.create(req.user, req.body);
+    sendResponse<Reviews>(res, {
+        statusCode: 200,
+        message: 'Successfully review Created !!',
+        success: true,
+        data: result
+    })
+})
+
+const getAllReview = catchAsync(async (req: Request, res: Response) => {
+    const result = await ReviewService.getAllReviews();
+    sendResponse<Reviews[]>(res, {
+        statusCode: 200,
+        message: 'Successfully Retrieve review !!',
+        success: true,
+        data: result,
+    })
+})
+
+const getDoctorReviews = catchAsync(async (req: Request, res: Response) => {
+    const result = await ReviewService.getDoctorReviews(req.params.id);
+    sendResponse<Reviews[]>(res, {
+        statusCode: 200,
+        message: 'Successfully Retrieve review !!',
+        success: true,
+        data: result,
+    })
+})
+
+
+const deleteReview = catchAsync(async (req: Request, res: Response) => {
+    const result = await ReviewService.deleteReviews(req.params.id);
+    sendResponse<Reviews>(res, {
+        statusCode: 200,
+        message: 'Successfully Deleted review !!',
+        success: true,
+        data: result,
+    })
+})
+
+const updateReview = catchAsync(async (req: Request, res: Response) => {
+    const result = await ReviewService.updateReview(req.params.id, req.body);
+    sendResponse<Reviews>(res, {
+        statusCode: 200,
+        message: 'Successfully Updated review !!',
+        success: true,
+        data: result,
+    })
+})
+
+export const ReviewController = {
+    creatReview,
+    updateReview,
+    getAllReview,
+    getDoctorReviews,
+    deleteReview
+}
