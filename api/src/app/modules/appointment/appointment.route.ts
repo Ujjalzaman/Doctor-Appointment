fdb1b4dcf206772f150b628dffc2be3c386ffe14
ujjalzaman <muzzwal97@gmail.com>
2023-12-03 12:47:16 +0600

    Implemented time slot api

--- api/src/app/modules/appointment/appointment.route.ts
+++ api/src/app/modules/appointment/appointment.route.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import { auth } from '../../middlewares/auth';
+import { AuthUser } from '../../../enums';
 import { AppointmentController } from './appointment.controller';
+import { UserRole } from '@prisma/client';
 
 const router = express.Router();
 
-router.get('/:id', AppointmentController.getSingleAppointment);
-router.post('/create', AppointmentController.createAppointment);
-router.get('/', AppointmentController.getAllAppointment);
-router.patch('/:id', AppointmentController.updateAppointment);
-router.delete('/:id', AppointmentController.deleteAppointment);
+router.get('/doctor', auth(AuthUser.DOCTOR), AppointmentController.doctorAppointment);
+router.get('/doctor/update-appointment', auth(AuthUser.DOCTOR), AppointmentController.updateAppointmentByDoctor);
+router.get('/patient', auth(AuthUser.PATIENT), AppointmentController.doctorAppointment);
 
 export const AppointmentRouter = router;
