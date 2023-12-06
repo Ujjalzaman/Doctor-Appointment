74ea1918c114d36a6c5c48186c3cb84b9ab9b12e
ujjalzaman <muzzwal97@gmail.com>
2023-12-07 00:20:27 +0600

    Fixed RTK query and resolve cors issue

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
 );
