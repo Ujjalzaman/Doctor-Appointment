c4d5e7f1ccbe414266f724dccffd1c4b6c1269a7
ujjalzaman <muzzwal97@gmail.com>
2023-12-10 23:47:17 +0600

    Implmented Patient Dashboard page

--- src/components/UI/DoctorDashboardSidebar.jsx
+++ src/components/UI/DoctorDashboardSidebar.jsx
@@ -2,79 +2,137 @@ import React from 'react';
 import img from '../../images/doc/doc4.jpg';
 import './DashboardSidebar.css';
 import { Link } from 'react-router-dom';
+import useAuthCheck from '../../redux/hooks/useAuthCheck';
 const DoctorDashboardSidebar = () => {
+    const { data, role } = useAuthCheck();
     return (
         <div className="profile-sidebar">
             <div className="widget-profile pro-widget-content">
-                <div className="profile-info-widget">
-                    <a href="#" className="booking-doc-img">
-                        <img src={img} alt="User Image" />
-                    </a>
-                    <div className="profile-det-info">
-                        <h3>Dr. Darren Elder</h3>
+                {
+                    role === 'doctor' ?
+                        <div className="profile-info-widget">
+                            <a className="booking-doc-img">
+                                <img src={img} alt="User Image" />
+                            </a>
+                            <div className="profile-det-info">
+                                <h3>{data?.firstName + " " + data?.lastName}</h3>
 
-                        <div className="patient-details">
-                            <h5 className="mb-0">BDS, MDS - Oral & Maxillofacial Surgery</h5>
+                                <div className="patient-details">
+                                    <h5 className="mb-0">BDS, MDS - Oral & Maxillofacial Surgery</h5>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                        :
+                        <div className="profile-info-widget">
+                            <a className="booking-doc-img">
+                                <img src={img} alt="User Image" />
+                            </a>
+                            <div className="profile-det-info">
+                                <h3>{data?.firstName + " " + data?.lastName}</h3>
+
+                                <div className="patient-details">
+                                    <p className='p-1 m-0'>24 Jul 1983, 38 Years</p>
+                                    <p className='p-1 m-0'> New Yourk , USA</p>
+                                    <p className='p-1 m-0'>{data?.email}</p>
+                                </div>
+                            </div>
+                        </div>
+                }
+
             </div>
             <div className="dashboard-widget">
                 <nav className="dashboard-menu">
-                    <ul>
-                        <li className="active">
-                            <Link to={'/doctor/dashboard'}>
-                                <i className="fas fa-columns"></i>
-                                <span>Dashboard</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={'/doctor/appointments'}>
-                                <i className="fas fa-calendar-check"></i>
-                                <span>Appointments</span>
-                            </Link>
+                    {
+                        role == 'patient' ?
+                            <ul>
+                                <li className="active">
+                                    <Link to={'/doctor/dashboard'}>
+                                        <i className="fas fa-columns"></i>
+                                        <span>Dashboard</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={'/doctor/appointments'}>
+                                        <i className="fas fa-calendar-check"></i>
+                                        <span>Favourites</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={'/doctor/profile-setting'}>
+                                        <i className="fas fa-user-cog"></i>
+                                        <span>Profile Settings</span>
+                                    </Link>
+                                </li>
 
-                        </li>
-                        <li>
-                            <Link to={'/doctor/my-patients'}>
-                                <i className="fas fa-user-injured"></i>
-                                <span>My Patients</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={'/doctor/schedule'}>
-                                <i className="fas fa-hourglass-start"></i>
-                                <span>Schedule Timings</span>
-                            </Link>
-                        </li>
+                                <li>
+                                    <Link to={'/doctor/change-password'}>
+                                        <i className="fas fa-lock"></i>
+                                        <span>Change Password</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <i className="fas fa-sign-out-alt"></i>
+                                        <span>Logout</span>
+                                    </a>
+                                </li>
+                            </ul>
+                            :
+                            <ul>
+                                <li className="active">
+                                    <Link to={'/doctor/dashboard'}>
+                                        <i className="fas fa-columns"></i>
+                                        <span>Dashboard</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={'/doctor/appointments'}>
+                                        <i className="fas fa-calendar-check"></i>
+                                        <span>Appointments</span>
+                                    </Link>
 
-                        <li>
-                            <Link to={'/doctor/reviews'}>
-                                <i className="fas fa-star"></i>
-                                <span>Reviews</span>
-                            </Link>
-                        </li>
+                                </li>
+                                <li>
+                                    <Link to={'/doctor/my-patients'}>
+                                        <i className="fas fa-user-injured"></i>
+                                        <span>My Patients</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={'/doctor/schedule'}>
+                                        <i className="fas fa-hourglass-start"></i>
+                                        <span>Schedule Timings</span>
+                                    </Link>
+                                </li>
 
-                        <li>
-                            <Link to={'/doctor/profile-setting'}>
-                                <i className="fas fa-user-cog"></i>
-                                <span>Profile Settings</span>
-                            </Link>
-                        </li>
+                                <li>
+                                    <Link to={'/doctor/reviews'}>
+                                        <i className="fas fa-star"></i>
+                                        <span>Reviews</span>
+                                    </Link>
+                                </li>
 
-                        <li>
-                            <Link to={'/doctor/change-password'}>
-                                <i className="fas fa-lock"></i>
-                                <span>Change Password</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <a href="">
-                                <i className="fas fa-sign-out-alt"></i>
-                                <span>Logout</span>
-                            </a>
-                        </li>
-                    </ul>
+                                <li>
+                                    <Link to={'/doctor/profile-setting'}>
+                                        <i className="fas fa-user-cog"></i>
+                                        <span>Profile Settings</span>
+                                    </Link>
+                                </li>
+
+                                <li>
+                                    <Link to={'/doctor/change-password'}>
+                                        <i className="fas fa-lock"></i>
+                                        <span>Change Password</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <i className="fas fa-sign-out-alt"></i>
+                                        <span>Logout</span>
+                                    </a>
+                                </li>
+                            </ul>
+                    }
                 </nav>
             </div>
         </div>
---
+++ src/components/UI/PatientAppointments.jsx
@@ -0,0 +1,224 @@
+import React, { useState } from 'react'
+import img from '../../images/john.png';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs'
+import { FaEye } from "react-icons/fa";
+
+const PatientAppointments = () => {
+    const [key, setKey] = useState('appointment');
+    return (
+        <Tabs
+            defaultActiveKey="appointment"
+            id="uncontrolled-tab-example"
+            className="mb-3"
+            onSelect={(k) => setKey(k)}
+        >
+            <Tab eventKey="appointment" title="Appointment">
+                <div className="appointment-tab">
+                    <div className="tab-content">
+                        <div className="tab-pane show active" id="upcoming-appointments">
+                            <div className="card card-table mb-0">
+                                <div className="card-body">
+                                    <div className="table-responsive">
+                                        <table className="table table-hover table-center mb-0">
+                                            <thead>
+                                                <tr>
+                                                    <th className='text-nowrap'>Doctor</th>
+                                                    <th className='text-nowrap'>Appt Date</th>
+                                                    <th className='text-nowrap'>Booking Date</th>
+                                                    <th className='text-nowrap'>Amount</th>
+                                                    <th className='text-nowrap'>Follow Up</th>
+                                                    <th className='text-nowrap'>Status</th>
+                                                    <th className="text-nowrap">Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr className='text-nowrap' >
+                                                    <td>
+                                                        <div className="table-avatar">
+                                                            <a className="avatar avatar-sm mr-2 d-flex gap-2">
+                                                                <img className="avatar-img rounded-circle" src={img} alt="User Image" />
+                                                                <div>
+                                                                    <p className='p-0 m-0 text-nowrap'>Dr. Ruby Perrin</p>
+                                                                    <p className='p-0 m-0'>Dental</p>
+                                                                </div>
+                                                            </a>
+                                                        </div>
+                                                    </td>
+                                                    <td className='text-nowrap'>11 Nov 2019 <span className="d-block text-info">10.00 AM</span></td>
+                                                    <td>11 Nov 2019</td>
+                                                    <td>$150</td>
+                                                    <td>11 Nov 2019</td>
+                                                    <td>
+                                                        <span class="badge rounded-pill text-bg-success">Confirm</span>
+                                                    </td>
+                                                    <td>
+                                                        <div className="table-action">
+                                                            <div className="btn btn-sm bg-info-light">
+                                                                <FaEye /> View
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Tab>
+            <Tab eventKey="Prescription" title="Prescription">
+                <div className="appointment-tab">
+                    <div className="tab-content">
+                        <div className="tab-pane show active" id="upcoming-appointments">
+                            <div className="card card-table mb-0">
+                                <div className="card-body">
+                                    <div className="table-responsive">
+                                        <table className="table table-hover table-center mb-0">
+                                            <thead>
+                                                <tr>
+                                                    <th className='text-nowrap'>Date</th>
+                                                    <th className='text-nowrap'>Title</th>
+                                                    <th className='text-nowrap'>App Doctor</th>
+                                                    <th className='text-nowrap'>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr className='text-nowrap' >
+                                                    <td>11 Nov 2019</td>
+                                                    <td>Fever Problem</td>
+                                                    <td>
+                                                        <div className="table-avatar">
+                                                            <a className="avatar avatar-sm mr-2 d-flex gap-2">
+                                                                <img className="avatar-img rounded-circle" src={img} alt="User Image" />
+                                                                <div>
+                                                                    <p className='p-0 m-0 text-nowrap'>Dr. Ruby Perrin</p>
+                                                                    <p className='p-0 m-0'>Dental</p>
+                                                                </div>
+                                                            </a>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div className="table-action">
+                                                            <div className="btn btn-sm bg-info-light">
+                                                                <FaEye /> View
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Tab>
+            <Tab eventKey="Records" title="Medical Records">
+                <div className="appointment-tab">
+                    <div className="tab-content">
+                        <div className="tab-pane show active" id="upcoming-appointments">
+                            <div className="card card-table mb-0">
+                                <div className="card-body">
+                                    <div className="table-responsive">
+                                        <table className="table table-hover table-center mb-0">
+                                            <thead>
+                                                <tr>
+                                                    <th className='text-nowrap'>ID</th>
+                                                    <th className='text-nowrap'>Date</th>
+                                                    <th className='text-nowrap'>Description</th>
+                                                    <th className='text-nowrap'>App Doctor</th>
+                                                    <th className='text-nowrap'>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr className='text-nowrap' >
+                                                    <td>#MR-00010</td>
+                                                    <td>14 Dec 2023</td>
+                                                    <td>To Much Fever</td>
+                                                    <td>
+                                                        <div className="table-avatar">
+                                                            <a className="avatar avatar-sm mr-2 d-flex gap-2">
+                                                                <img className="avatar-img rounded-circle" src={img} alt="User Image" />
+                                                                <div>
+                                                                    <p className='p-0 m-0 text-nowrap'>Dr. Ruby Perrin</p>
+                                                                    <p className='p-0 m-0'>Dental</p>
+                                                                </div>
+                                                            </a>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div className="table-action">
+                                                            <div className="btn btn-sm bg-info-light">
+                                                                <FaEye /> View
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Tab>
+            <Tab eventKey="Billing" title="Billing">
+            <div className="appointment-tab">
+                    <div className="tab-content">
+                        <div className="tab-pane show active" id="upcoming-appointments">
+                            <div className="card card-table mb-0">
+                                <div className="card-body">
+                                    <div className="table-responsive">
+                                        <table className="table table-hover table-center mb-0">
+                                            <thead>
+                                                <tr>
+                                                    <th className='text-nowrap'>Invoice No</th>
+                                                    <th className='text-nowrap'>Doctor</th>
+                                                    <th className='text-nowrap'>Amount</th>
+                                                    <th className='text-nowrap'>Paid On</th>
+                                                    <th className='text-nowrap'>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr className='text-nowrap' >
+                                                    <td>#INV-00010</td>
+                                                    <td>
+                                                    <div className="table-avatar">
+                                                            <a className="avatar avatar-sm mr-2 d-flex gap-2">
+                                                                <img className="avatar-img rounded-circle" src={img} alt="User Image" />
+                                                                <div>
+                                                                    <p className='p-0 m-0 text-nowrap'>Dr. Ruby Perrin</p>
+                                                                    <p className='p-0 m-0'>Dental</p>
+                                                                </div>
+                                                            </a>
+                                                        </div>
+                                                    </td>
+                                                    <td>150$</td>
+                                                    <td>14 Dec 2023</td>
+                                                    <td>
+                                                        <div className="table-action">
+                                                            <div className="btn btn-sm bg-info-light">
+                                                                <FaEye /> View
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Tab>
+        </Tabs>
+    )
+}
+
+export default PatientAppointments
