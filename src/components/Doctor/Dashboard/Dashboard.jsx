345d3def8919259c16de41cc8435225799f4774c
ujjalzaman <muzzwal97@gmail.com>
2023-12-10 23:47:17 +0600

    Implmented Patient Dashboard page

--- src/components/Doctor/Dashboard/Dashboard.jsx
+++ src/components/Doctor/Dashboard/Dashboard.jsx
@@ -2,22 +2,33 @@ import React from 'react'
 import DashboardLayout from '../DashboardLayout/DashboardLayout'
 import DoctorDashCard from '../../UI/DoctorDashCard'
 import PatientAppointment from '../../UI/PatientAppointment'
+import useAuthCheck from '../../../redux/hooks/useAuthCheck'
+import PatientAppointments from '../../UI/PatientAppointments'
 
 const MainDashboard = () => {
+    const { authChecked, data, role } = useAuthCheck();
     return (
         <>
             <DashboardLayout>
                 <div className="row">
                     <div className="col-md-12">
-                        <DoctorDashCard />
+                        {role === 'doctor' && <DoctorDashCard />}
                     </div>
                 </div>
 
                 <div className="row">
-                    <div className="col-md-12">
-                        <h4 className="mb-4">Patient Appoinment</h4>
-                        <PatientAppointment />
-                    </div>
+                    {role === 'patient' ?
+                        <div className="col-md-12">
+                            <h4 className="mb-4">My Appointments</h4>
+                            <PatientAppointments />
+                        </div>
+                        :
+                        <div className="col-md-12">
+                            <h4 className="mb-4">Patient Appoinments</h4>
+                            <PatientAppointment />
+                        </div>
+                    }
+
                 </div>
             </DashboardLayout>
         </>
