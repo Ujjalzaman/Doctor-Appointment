79ccd2fac6e149ce7b205228e073f49724787fe2
ujjalzaman <muzzwal97@gmail.com>
2023-12-15 01:53:42 +0600

    Implemented Patient profile update form and intregated with the api

--- src/redux/tag-types.js
+++ src/redux/tag-types.js
@@ -1,9 +1,11 @@
 export const tagTypes = {
     favourite: 'favourite',
     prescription: 'prescription',
+    patient: 'patient',
 }
 
 export const tagTypeList = [
     tagTypes.favourite,
-    tagTypes.prescription
+    tagTypes.prescription,
+    tagTypes.patient
 ]
