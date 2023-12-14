2a2a82419d9fb5cde2059a48c9e3d476873e1173
ujjalzaman <muzzwal97@gmail.com>
2023-12-15 01:53:42 +0600

    Implemented Patient profile update form and intregated with the api

--- api/src/app/modules/patient/patient.route.ts
+++ api/src/app/modules/patient/patient.route.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { PatientController } from './patient.controller';
+import { auth } from '../../middlewares/auth';
+import { AuthUser } from '../../../enums';
 
 const router = express.Router();
 
@@ -7,6 +9,6 @@ router.get('/', PatientController.getAllPatients);
 router.post('/', PatientController.createPatient);
 router.get('/:id', PatientController.getPatient);
 router.delete('/:id', PatientController.deletePatient);
-router.patch('/:id', PatientController.updatePatient);
+router.patch('/:id',auth(AuthUser.PATIENT), PatientController.updatePatient);
 
 export const PatientRouter = router;
