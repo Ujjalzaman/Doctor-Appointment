28886575031732d45f1620ba958c1d6b939e8f32
ujjalzaman <muzzwal97@gmail.com>
2023-12-17 23:56:30 +0600

    Intregated appointments api

--- src/components/Doctor/Appointments/Appointments.jsx
+++ src/components/Doctor/Appointments/Appointments.jsx
@@ -2,44 +2,53 @@ import React from 'react'
 import DashboardLayout from '../DashboardLayout/DashboardLayout'
 import img from '../../../images/john.png';
 import './Appointments.css';
+import { useGetDoctorAppointmentsQuery } from '../../../redux/api/appointmentApi';
+import moment from 'moment';
 
 const Appointments = () => {
-    return (
-        <DashboardLayout>
-            {/* <div className="col-md-7 col-lg-8 col-xl-9"> */}
-                <div className="appointments">
-                    <div className="appointment-list">
-                        <div className="profile-info-widget">
-                            <a href="#" className="booking-doc-img">
-                                <img src={img} alt="User Image" />
-                            </a>
-                            <div className="profile-det-info">
-                                <h3><a href="patient-profile.html">Richard Wilson</a></h3>
-                                <div className="patient-details">
-                                    <h5><i className="far fa-clock"></i> 14 Nov 2019, 10.00 AM</h5>
-                                    <h5><i className="fas fa-map-marker-alt"></i> Newyork, United States</h5>
-                                    <h5><i className="fas fa-envelope"></i> richard@example.com</h5>
-                                    <h5 className="mb-0"><i className="fas fa-phone"></i> +1 923 782 4575</h5>
+    const { data, isError, isLoading } = useGetDoctorAppointmentsQuery({});
+    let content = null;
+    if (!isLoading && isError) content = <div>Something Went Wrong !</div>
+    if (!isLoading && !isError && data?.length === 0) content = <div>Empty</div>
+    if (!isLoading && !isError && data?.length > 0) content =
+        <>
+            {
+                data && data.map((item) => (
+                    <div className="appointments">
+                        <div className="appointment-list">
+                            <div className="profile-info-widget">
+                                <a href="#" className="booking-doc-img">
+                                    <img src={img} alt="User Image" />
+                                </a>
+                                <div className="profile-det-info">
+                                    <h3><a href="patient-profile.html">{item?.patient?.firstName + ' ' + item?.patient?.lastName}</a></h3>
+                                    <div className="patient-details">
+                                        <h5><i className="far fa-clock"></i> {moment(item?.appointmentTime).format("MMM Do YY")} </h5>
+                                        <h5><i className="fas fa-map-marker-alt"></i> {item?.patient?.address}</h5>
+                                        <h5><i className="fas fa-envelope"></i> {item?.patient?.email}</h5>
+                                        <h5 className="mb-0"><i className="fas fa-phone"></i> {item?.patient?.mobile}</h5>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="appointment-action">
-                            <a href="#" className="btn btn-sm bg-info-light" data-toggle="modal" data-target="#appt_details">
-                                <i className="far fa-eye"></i> View
-                            </a>
-                            <a  className="btn btn-sm bg-success-light">
-                                <i className="fas fa-check"></i> Accept
-                            </a>
-                            <a  className="btn btn-sm bg-danger-light">
-                                <i className="fas fa-times"></i> Cancel
-                            </a>
+                            <div className="appointment-action">
+                                <a href="#" className="btn btn-sm bg-info-light" data-toggle="modal" data-target="#appt_details">
+                                    <i className="far fa-eye"></i> View
+                                </a>
+                                <a className="btn btn-sm bg-success-light">
+                                    <i className="fas fa-check"></i> Accept
+                                </a>
+                                <a className="btn btn-sm bg-danger-light">
+                                    <i className="fas fa-times"></i> Cancel
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            {/* </div> */}
-
-
-
+                ))
+            }
+        </>
+    return (
+        <DashboardLayout>
+            {content}
         </DashboardLayout>
     )
 }
