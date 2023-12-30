d4721c1b5648944363012d6aacd5a62d5480e03c
ujjalzaman <muzzwal97@gmail.com>
2023-12-30 16:10:52 +0600

    Implemented Stepper to collect booking info and Updated ui and manage state

--- src/components/Booking/DoctorBooking/DoctorBooking.jsx
+++ src/components/Booking/DoctorBooking/DoctorBooking.jsx
@@ -4,18 +4,23 @@ import Footer from '../../Shared/Footer/Footer'
 import img from '../../../images/doc/doctor 3.jpg'
 import './index.css';
 import BreadCrumb from '../../UI/BreadCrumb';
-import { Link, useParams } from 'react-router-dom';
-import { Empty, Button,DatePicker } from 'antd';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Empty, Button, message, Steps } from 'antd';
 import { useGetDoctorQuery } from '../../../redux/api/doctorApi';
 import { FaArchway } from "react-icons/fa";
 import { useGetAppointmentTimeQuery } from '../../../redux/api/timeSlotApi';
 import moment from 'moment';
+import SelectDateAndTime from './SelectDateAndTime';
+import PersonalInformation from '../BookingCheckout/PersonalInformation';
+import CheckoutPage from '../BookingCheckout/CheckoutPage';
 
 const DoctorBooking = () => {
+    const [current, setCurrent] = useState(0);
     const [selectedDate, setSelectedDate] = useState('');
     const [selectDay, setSelecDay] = useState('');
     const [selectTime, setSelectTime] = useState('');
     const { doctorId } = useParams();
+    const navigation = useNavigate();
     const { data, isLoading, isError, error } = useGetDoctorQuery(doctorId);
     const { data: time, refetch, isLoading: dIsLoading, isError: dIsError, error: dError } = useGetAppointmentTimeQuery({ day: selectDay, id: doctorId });
 
@@ -24,13 +29,10 @@ const DoctorBooking = () => {
         setSelecDay(moment(dateString).format('dddd').toLowerCase());
         refetch();
     }
-    const disabledDateTime = (current) => {
-        return current && (current < moment().add(1, 'day').startOf('day') || current > moment().add(8, 'days').startOf("day"))
-    }
-
-    const handleSelectTime = (date) => {
-        setSelectTime(date)
-    }
+    const disabledDateTime = (current) => current && (current < moment().add(1, 'day').startOf('day') || current > moment().add(8, 'days').startOf("day"))
+    const handleSelectTime = (date) => { setSelectTime(date) }
+    const next = () => { setCurrent(current + 1) };
+    const prev = () => { setCurrent(current - 1) };
 
     let dContent = null;
     if (dIsLoading) dContent = <div>Loading ...</div>
@@ -41,7 +43,7 @@ const DoctorBooking = () => {
             {
                 time && time.map((item) => (
                     <div className="col-md-4">
-                        <Button type={item?.slot?.time=== selectTime ? "primary" : "default"} shape="round" size='large' className='mb-3' onClick={() => handleSelectTime(item?.slot?.time)}> {item?.slot?.time} </Button>
+                        <Button type={item?.slot?.time === selectTime ? "primary" : "default"} shape="round" size='large' className='mb-3' onClick={() => handleSelectTime(item?.slot?.time)}> {item?.slot?.time} </Button>
                     </div>
                 ))
             }
@@ -50,8 +52,8 @@ const DoctorBooking = () => {
     //What to render
     let content = null;
     if (!isLoading && isError) content = <div>Something Went Wrong!</div>
-    if (!isLoading && !isError && data.id === undefined) content = <Empty />
-    if (!isLoading && !isError && data.id) content =
+    if (!isLoading && !isError && data?.id === undefined) content = <Empty />
+    if (!isLoading && !isError && data?.id) content =
         <>
             <div className="card shadow-sm mb-3">
                 <div className="card-body">
@@ -69,42 +71,48 @@ const DoctorBooking = () => {
                 </div>
             </div>
         </>
+
+    const steps = [
+        {
+            title: 'Select Appointment Date & Time',
+            content: <SelectDateAndTime
+                content={content}
+                handleDateChange={handleDateChange}
+                disabledDateTime={disabledDateTime}
+                selectedDate={selectedDate}
+                dContent={dContent}
+                selectTime={selectTime}
+            />
+        },
+        {
+            title: 'Patient Information',
+            content: <PersonalInformation />
+        },
+        {
+            title: 'Payment',
+            content: <CheckoutPage />,
+        },
+    ]
+
+    const items = steps.map((item) => ({
+        key: item.title,
+        title: item.title,
+    }))
+
+    const handleConfirmSchedule = () =>{
+        message.success('Processing complete!');
+        navigation('/booking/success')
+    }
     return (
         <>
             <Navbar />
             <BreadCrumb />
-            <div className="content">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            {content}
-                            <div className="card py-2 d-flex justify-content-between" style={{ height: '70vh' }}>
-                                <div className="row m-2">
-                                    <div className="col-6">
-                                        <DatePicker
-                                            format="YYYY-MM-DD HH:mm:ss"
-                                            disabledDate={disabledDateTime}
-                                            open={true}
-                                            onChange={handleDateChange}
-                                        />
-                                    </div>
-                                    <div className="col-6">
-                                        {selectedDate ? <h4>Schedule Date: {selectedDate && moment(selectedDate).format('LL')} 
-                                        {selectTime && 'time :' + selectTime}</h4>: "Please Select Date First"}
-                                        <div className="schedule-cont">
-                                            <div className="row">
-                                                {dContent}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="submit-section proceed-btn text-end">
-                                <Link className="btn btn-primary submit-btn mt-2" to={"/booking/checkout"}>Next</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <Steps current={current} items={items} />
+            <div className='mb-5 mt-3 mx-3'>{steps[current].content}</div>
+            <div className='text-end mx-3' style={{marginBottom: 48 }} >
+                {current < steps.length - 1 && (<Button type="primary" onClick={() => next()}>Next</Button>)}
+                {current === steps.length - 1 && (<Button type="primary" onClick={() => handleConfirmSchedule()}>Confirm</Button>)}
+                {current > 0 && (<Button style={{ margin: '0 8px', }} onClick={() => prev()} >Previous</Button>)}
             </div>
             <Footer />
         </>
