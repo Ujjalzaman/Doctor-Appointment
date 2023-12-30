1345153cf18918ccfcba0d20b089ed65f1887999
ujjalzaman <muzzwal97@gmail.com>
2023-12-31 00:57:36 +0600

    Allocate Booking Information Added Error handling and date format updated

--- src/components/Booking/BookingCheckout/CheckoutPage.jsx
+++ src/components/Booking/BookingCheckout/CheckoutPage.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import moment from 'moment';
 import img from '../../../images/doc/doctor 3.jpg';
 import './BookingCheckout.css';
 
-const CheckoutPage = () => {
+const CheckoutPage = ({ handleChange, selectValue, isCheck, setIsChecked, data, selectedDate, selectTime }) => {
+    const handleCheck = () => {
+        setIsChecked(!isCheck)
+    }
     return (
         <div className="container">
             <div className="row">
@@ -10,55 +13,93 @@ const CheckoutPage = () => {
                     <div className="payment-widget">
 
                         <div className="payment-list">
-                            <label className="payment-radio credit-card-option">
-                                <input type="radio" name="radio" checked />
-                                <span className="checkmark"></span>
-                                Credit card
-                            </label>
                             <div className="row">
                                 <div className="col-md-6">
+                                    <label className="payment-radio credit-card-option">
+                                        <input type="radio"
+                                            name="paymentType"
+                                            value="creditCard"
+                                            onChange={(e) => handleChange(e)}
+                                            checked={selectValue?.paymentType === 'creditCard'}
+                                        />
+                                        <span className="checkmark"></span>
+                                        Credit card
+                                    </label>
+                                </div>
+                                <div className="col-md-6">
+                                    <label className="payment-radio credit-card-option">
+                                        <input type="radio"
+                                            name="paymentType"
+                                            value="cash"
+                                            onChange={(e) => handleChange(e)}
+                                            checked={selectValue?.paymentType === 'cash'}
+                                        />
+                                        <span className="checkmark"></span>
+                                        Cash
+                                    </label>
+                                </div>
+                                <div className="col-md-6">
                                     <div className="form-group card-label">
-                                        <label for="card_name">Name on Card</label>
-                                        <input className="form-control" id="card_name" type="text" />
+                                        <label htmlFor="card_name">Name on Card</label>
+                                        <input className="form-control" id="card_name" type="text" onChange={(e) => handleChange(e)} name='nameOnCard' />
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className="form-group card-label">
-                                        <label for="card_number">Card Number</label>
-                                        <input className="form-control" id="card_number" placeholder="1234  5678  9876  5432" type="text" />
+                                        <label htmlFor="card_number">Card Number</label>
+                                        <input className="form-control" id="card_number" placeholder="1234  5678  9876  5432" type="number" onChange={(e) => handleChange(e)} name='cardNumber' />
                                     </div>
                                 </div>
                                 <div className="col-md-4">
                                     <div className="form-group card-label">
-                                        <label for="expiry_month">Expiry Month</label>
-                                        <input className="form-control" id="expiry_month" placeholder="MM" type="text" />
+                                        <label htmlFor="expiry_month">Expiry Month</label>
+                                        <input className="form-control" id="expiry_month" placeholder="MM" type="number" onChange={(e) => handleChange(e)} name='expiredMonth' />
                                     </div>
                                 </div>
                                 <div className="col-md-4">
                                     <div className="form-group card-label">
-                                        <label for="expiry_year">Expiry Year</label>
-                                        <input className="form-control" id="expiry_year" placeholder="YY" type="text" />
+                                        <label htmlFor="expiry_year">Expiry Year</label>
+                                        <input className="form-control" id="expiry_year" placeholder="YY" type="number" onChange={(e) => handleChange(e)} name='cardExpiredYear' />
                                     </div>
                                 </div>
                                 <div className="col-md-4">
                                     <div className="form-group card-label">
-                                        <label for="cvv">CVV</label>
-                                        <input className="form-control" id="cvv" type="text" />
+                                        <label htmlFor="cvv">CVV</label>
+                                        <input className="form-control" id="cvv" type="number" onChange={(e) => handleChange(e)} name='cvv' />
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="payment-list">
+                        <div className="payment-list d-flex gap-2 mt-3">
                             <label className="payment-radio paypal-option">
-                                <input type="radio" name="radio" />
+                                <input type="radio"
+                                    name="paymentMethod"
+                                    value="paypal"
+                                    onChange={(e) => handleChange(e)}
+                                    checked={selectValue?.paymentMethod === 'paypal'}
+                                />
                                 <span className="checkmark"></span>
                                 Paypal
+                            </label>
+                            <label className="payment-radio paypal-option">
+                                <input type="radio"
+                                    name="paymentMethod"
+                                    value="payoneer"
+                                    onChange={(e) => handleChange(e)}
+                                    checked={selectValue?.paymentMethod === 'payoneer'}
+                                />
+                                <span className="checkmark"></span>
+                                Payoneer
                             </label>
                         </div>
                         <div className="terms-accept">
                             <div className="custom-checkbox">
-                                <input type="checkbox" id="terms_accept" />
-                                <label for="terms_accept">I have read and accept <a href="#">Terms &amp; Conditions</a></label>
+                                <input
+                                    type="checkbox"
+                                    id="terms_accept" className='me-2'
+                                    checked={isCheck}
+                                    onChange={handleCheck} />
+                                <label htmlFor="terms_accept"> I have read and accept <a className='text-primary' style={{cursor:'pointer', textDecoration:'none'}}>Terms &amp; Conditions</a></label>
                             </div>
                         </div>
                     </div>
@@ -71,17 +112,11 @@ const CheckoutPage = () => {
                                     <img src={img} alt="" />
                                 </a>
                                 <div className="booking-info">
-                                    <h4><a href="doctor-profile.html">Dr. Darren Elder</a></h4>
-                                    <div className="rating">
-                                        <i className="fas fa-star filled"></i>
-                                        <i className="fas fa-star filled"></i>
-                                        <i className="fas fa-star filled"></i>
-                                        <i className="fas fa-star filled"></i>
-                                        <i className="fas fa-star"></i>
-                                        <span className="d-inline-block average-rating">35</span>
-                                    </div>
+                                    <h4><a>Dr. {data?.firstName + ' ' + data?.lastName}</a></h4>
+                                   
                                     <div className="clinic-details">
-                                        <p className="doc-location"><i className="fas fa-map-marker-alt"></i> Newyork, USA</p>
+                                        <p className="doc-location"><i className="fas fa-map-marker-alt"></i> {data?.designation}</p>
+                                        <p className="doc-location"><i className="fas fa-map-marker-alt"></i> {data?.clinicAddress}</p>
                                     </div>
                                 </div>
                             </div>
@@ -89,19 +124,18 @@ const CheckoutPage = () => {
                             <div className="booking-summary">
                                 <div className="booking-item-wrap">
                                     <ul className="booking-date">
-                                        <li>Date <span>16 Nov 2019</span></li>
-                                        <li>Time <span>10:00 AM</span></li>
+                                        <li>Date <span>{moment(selectedDate).format('LL')}</span></li>
+                                        <li>Time <span>{selectTime}</span></li>
                                     </ul>
                                     <ul className="booking-fee">
-                                        <li>Consulting Fee <span>$100</span></li>
+                                        <li>Consulting Fee <span>${data?.price}</span></li>
                                         <li>Booking Fee <span>$10</span></li>
-                                        <li>Video Call <span>$50</span></li>
                                     </ul>
                                     <div className="booking-total">
                                         <ul className="booking-total-list">
                                             <li>
                                                 <span>Total</span>
-                                                <span className="total-cost">$160</span>
+                                                <span className="total-cost">${(Number(data?.price) + 10)}</span>
                                             </li>
                                         </ul>
                                     </div>
--- src/components/Booking/BookingCheckout/PersonalInformation.jsx
+++ src/components/Booking/BookingCheckout/PersonalInformation.jsx
@@ -1,38 +1,37 @@
 import './BookingCheckout.css';
 
-const PersonalInformation = () => {
+const PersonalInformation = ({handleChange, selectValue}) => {
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-12 col-lg-12">
                     <div className="card">
                         <div className="card-body">
-                            <form action="">
-
+                            <form>
                                 <div className="info-widget">
                                     <div className="row">
                                         <div className="col-md-6 col-sm-12">
                                             <div className="form-group card-label">
                                                 <label>First Name</label>
-                                                <input className="form-control" type="text" />
+                                                <input onChange={(e) => handleChange(e)} name='firstName' value={selectValue?.firstName} className="form-control" type="text"/>
                                             </div>
                                         </div>
                                         <div className="col-md-6 col-sm-12">
                                             <div className="form-group card-label">
                                                 <label>Last Name</label>
-                                                <input className="form-control" type="text" />
+                                                <input onChange={(e) => handleChange(e)} name='lastName' value={selectValue?.lastName} className="form-control" type="text"/>
                                             </div>
                                         </div>
                                         <div className="col-md-6 col-sm-12">
                                             <div className="form-group card-label">
                                                 <label>Email</label>
-                                                <input className="form-control" type="email" />
+                                                <input onChange={(e) => handleChange(e)} name='email' value={selectValue?.email} className="form-control" type="email"/>
                                             </div>
                                         </div>
                                         <div className="col-md-6 col-sm-12">
                                             <div className="form-group card-label">
                                                 <label>Phone</label>
-                                                <input className="form-control" type="text" />
+                                                <input onChange={(e) => handleChange(e)} name='phone' value={selectValue?.phone} className="form-control" type="text"/>
                                             </div>
                                         </div>
                                     </div>
