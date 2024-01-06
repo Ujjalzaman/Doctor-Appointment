2f01a03646d6cafd49d1ddc8845ffc58b225c9e4
ujjalzaman <muzzwal97@gmail.com>
2024-01-06 18:30:54 +0600

    Updated Home Page layout UI

--- src/components/Home/ClinicAndSpecialities/ClinicAndSpecialities.jsx
+++ src/components/Home/ClinicAndSpecialities/ClinicAndSpecialities.jsx
@@ -4,62 +4,62 @@ import img2 from '../../../images/specialities/specialities-02.png';
 import img3 from '../../../images/specialities/specialities-03.png';
 import img4 from '../../../images/specialities/specialities-04.png';
 import img5 from '../../../images/specialities/specialities-05.png';
+import { FaCheckDouble } from "react-icons/fa";
 import './index.css';
 
 const ClinicAndSpecialities = () => {
-    return (
-        <section className="section section-specialities position-relative">
-				<div className="container-fluid">
-					<div className="section-header text-center mb-5">
-						<h2>Clinic and Specialities</h2>
-						<p className="sub-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-					</div>
-					<div className="row justify-content-center">
-						<div className="col-md-9">
-							<div className="specialities-slider slider d-flex justify-content-center align-items-center gap-5">
-							
-							
-								<div className="speicality-item text-center">
-									<div className="speicality-img">
-										<img src={img1} className="img-fluid" alt=""/>
-										<span><i className="fa fa-circle" aria-hidden="true"></i></span>
-									</div>
-									<p>Urology</p>
-								</div>	
-								<div className="speicality-item text-center">
-									<div className="speicality-img">
-										<img src={img2} className="img-fluid" alt=""/>
-										<span><i className="fa fa-circle" aria-hidden="true"></i></span>
-									</div>
-									<p>Neurology</p>	
+	return (
+		<section className="section section-specialities position-relative">
+			<div className="container-fluid">
+				<div className='mb-5 section-title text-center'>
+					<h2>Clinic and Specialities</h2>
+					<p className='m-0'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+				</div>
+
+				<div className="row justify-content-center">
+					<div className="col-md-9">
+						<div className="specialities-slider slider d-flex justify-content-center align-items-center gap-5">
+							<div className="speicality-item text-center">
+								<div className="speicality-img">
+									<img src={img1} className="img-fluid" alt="" />
+									<span><i><FaCheckDouble/></i></span>
 								</div>
-								<div className="speicality-item text-center">
-									<div className="speicality-img">
-										<img src={img3} className="img-fluid" alt=""/>
-										<span><i className="fa fa-circle" aria-hidden="true"></i></span>
-									</div>
-									<p>Orthopedic</p>	
-								</div>				
-								<div className="speicality-item text-center">
-									<div className="speicality-img">
-										<img src={img4} className="img-fluid" alt=""/>
-										<span><i className="fa fa-circle" aria-hidden="true"></i></span>
-									</div>	
-									<p>Cardiologist</p>	
-								</div>			
-								<div className="speicality-item text-center">
-									<div className="speicality-img">
-										<img src={img5} className="img-fluid" alt=""/>
-										<span><i className="fa fa-circle" aria-hidden="true"></i></span>
-									</div>	
-									<p>Dentist</p>
-								</div>					
+								<p>Urology</p>
+							</div>
+							<div className="speicality-item text-center">
+								<div className="speicality-img">
+									<img src={img2} className="img-fluid" alt="" />
+									<span><i><FaCheckDouble/></i></span>
+								</div>
+								<p>Neurology</p>
+							</div>
+							<div className="speicality-item text-center">
+								<div className="speicality-img">
+									<img src={img3} className="img-fluid" alt="" />
+									<span><i><FaCheckDouble/></i></span>
+								</div>
+								<p>Orthopedic</p>
+							</div>
+							<div className="speicality-item text-center">
+								<div className="speicality-img">
+									<img src={img4} className="img-fluid" alt="" />
+									<span><i><FaCheckDouble/></i></span>
+								</div>
+								<p>Cardiologist</p>
+							</div>
+							<div className="speicality-item text-center">
+								<div className="speicality-img">
+									<img src={img5} className="img-fluid" alt="" />
+									<span><i><FaCheckDouble/></i></span>
+								</div>
+								<p>Dentist</p>
 							</div>
 						</div>
 					</div>
-				</div>   
-			</section>	 
-    );
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default ClinicAndSpecialities;
--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Appointment from '../Appointment/Appointment';
 import Blog from '../Blog/Blog';
 import Doctor from '../Doctor/Doctor';
-import FeaturedService from '../FeaturedService/FeaturedService';
 import Footer from '../../Shared/Footer/Footer';
-import Services from '../Services/Services';
 import Testimonial from '../Testimonial/Testimonial';
 import ClinicAndSpecialities from '../ClinicAndSpecialities/ClinicAndSpecialities';
 import BookDoctor from '../bookDoctor/BookDoctor';
@@ -12,6 +9,7 @@ import Availabe from '../AvailableFeatures/Available';
 import HeroSection from '../HeroSection/HeroSection';
 import InfoPage from '../InfoPage/InfoPage';
 import Header from '../../Shared/Header/Header';
+import Service from '../Services/Service';
 
 const Home = () => {
     return (
@@ -19,11 +17,9 @@ const Home = () => {
             <Header />
             <HeroSection />
             <InfoPage />
-            <Services />
+            <Service />
             <ClinicAndSpecialities />
             <BookDoctor />
-            <FeaturedService />
-            <Appointment />
             <Testimonial />
             <Blog />
             <Availabe />
