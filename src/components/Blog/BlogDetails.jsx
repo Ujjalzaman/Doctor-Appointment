1e53d3f9159478111eaba401f8ed630288afa16d
ujjalzaman <muzzwal97@gmail.com>
2024-01-07 21:48:37 +0600

    Updated UI of blog page , blog details and aside

--- src/components/Blog/BlogDetails.jsx
+++ src/components/Blog/BlogDetails.jsx
@@ -3,11 +3,14 @@ import { useParams } from 'react-router-dom'
 import { useGetSingleBlogQuery } from '../../redux/api/blogApi';
 import { Empty, message } from 'antd';
 import BlogAside from './BlogAside';
-import img from '../../images/cavity.png';
+import img from '../../images/chair.png';
 import dayjs from 'dayjs';
 import Footer from '../Shared/Footer/Footer';
 import BlogComment from './BlogComment';
 import Header from '../Shared/Header/Header';
+import SubHeader from '../Shared/SubHeader';
+import { FaRegUser, FaBusinessTime } from "react-icons/fa";
+import { FaFacebookSquare, FaInstagramSquare, FaLinkedin } from "react-icons/fa";
 
 const BlogDetails = () => {
     const { id } = useParams();
@@ -17,7 +20,7 @@ const BlogDetails = () => {
     if (!isLoading && isError) content = <div>{message.error('Something went Wrong!')}</div>
     if (!isLoading && !isError && data?.id === undefined) content = <Empty />
     if (!isLoading && !isError && data?.id) content =
-        <div className="card shadow text-center border-0 rounded-bottom">
+        <div className="card shadow-sm text-center border-0 rounded-bottom">
 
             <div className="flex-column card-header p-0 border-0 d-flex justify-content-center align-items-center" style={{ overflow: 'hidden', maxHeight: '40rem' }}>
                 <img src={img} alt="blog Image" width={800} height={500} className="image-hover w-100 rounded-top" style={{ objectFit: 'cover' }} />
@@ -25,65 +28,60 @@ const BlogDetails = () => {
 
             <div className="card-body p-0">
                 <div className="p-2 my-2">
-                    <h5 className="text-black text-start mb-1 text-primary"> {data?.title}</h5>
+                    <h5 className="text-start mb-1" style={{ color: '#05335c' }}>{data?.title}</h5>
                     <div className="d-flex text-start gap-2">
                         <div className="d-flex gap-1 text-muted align-items-center justify-content-center">
-                            <i className="ri-user-3-line"></i>
+                            <FaRegUser className='form-text' />
                             <span className="form-text">Ujjal zaman</span>
                         </div>
                         <div className="d-flex gap-1 text-muted align-items-center justify-content-center">
-                            <i className="ri-calendar-line"></i>
+                            <FaBusinessTime className='form-text' />
                             <span className="form-text">{dayjs(data?.cretedAt).format('MMM D, YYYY hh:mm A')}</span>
                         </div>
                     </div>
                     <hr className="my-1 p-0" />
                 </div>
                 <div className="px-3 my-3">
-                    <p className="form-text text-start">{data?.description}</p>
+                    <p className="text-secondary text-start">{data?.description}</p>
                 </div>
             </div>
-            <div className="d-flex gap-2 ms-2">
+            <div className="d-flex gap-2 ms-2 px-2 py-3">
                 <span className="">Tags:</span>
-                <div className="text-dark d-flex gap-2" style={{ fontWeight: '900' }}>
-                    <p>#Assembling</p>
-                    <p>#blog</p>
-                    <p>#Sport</p>
-                    <p>#Others</p>
+                <div className="d-flex gap-2">
+                    {
+                        Array(4).fill(null).map((_item, index) => (<h5 key={index + 2}>#tag{index}</h5>))
+                    }
                 </div>
             </div>
         </div>
     return (
         <>
             <Header />
-            <div className="container-full">
-                <div className="row mx-2" style={{ marginTop: '4rem', marginBottom: '8rem' }}>
-                    <div className="col-md-9">
+            <SubHeader title='Blog Details' subtitle='Lorem ipsum dolor sit amet.' />
+            <div className="container-fluid" style={{ marginTop: 150 }}>
+                <div className="row mx-2">
+                    <div className="col-md-9 col-sm-12">
                         {
                             content
                         }
                         <hr />
-                        <div className="d-flex justify-content-end shadow">
+                        <div className="d-flex justify-content-end">
                             <div className="col-md-5 col-lg-4 ml-lg-0 text-end text-md-end">
-                                <p className="text-dark rounded d-inline me-2">Share On </p>
-                                <a className="btn btn-outline-primary btn-floating m-1">
-                                    <i className="ri-facebook-fill"></i>
-                                </a>
-
+                                <h5 className="text-dark rounded d-inline me-2">Share On </h5>
                                 <a className="btn btn-outline-primary btn-floating m-1" >
-                                    <i className="ri-twitter-line"></i>
-                                </a>
-
-                                <a className="btn btn-outline-primary btn-floating m-1">
-                                    <i className="ri-google-line"></i>
+                                    <FaFacebookSquare />
                                 </a>
                                 <a className="btn btn-outline-primary btn-floating m-1">
-                                    <i className="ri-instagram-line"></i>
+                                    <FaInstagramSquare />
+                                </a>
+                                <a className="btn btn-outline-primary btn-floating m-1">
+                                    <FaLinkedin />
                                 </a>
                             </div>
                         </div>
-                        <BlogComment/>
+                        <BlogComment />
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-3 col-sm-12">
                         <BlogAside setSearchTerm={undefined} />
                     </div>
                 </div>
--- src/components/Home/Services/Service.jsx
+++ src/components/Home/Services/Service.jsx
@@ -7,29 +7,29 @@ import { Link } from 'react-router-dom';
 
 const Service = () => {
     return (
-        <section class="container" style={{marginTop: 200, marginBottom:200}}>
+        <section className="container" style={{marginTop: 200, marginBottom:200}}>
             <div className='mb-5 section-title text-center'>
                 <h2>Services</h2>
                 <p className='m-0'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
             </div>
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-4 col-sm-6">
-                        <div class="service-img">
-                            <img src={img} alt="" class="img-fluid" />
-                            <img src={img2} alt="" class="img-fluid mt-4" />
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-lg-4 col-sm-6">
+                        <div className="service-img">
+                            <img src={img} alt="" className="img-fluid" />
+                            <img src={img2} alt="" className="img-fluid mt-4" />
                         </div>
                     </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <div class="service-img mt-4 mt-lg-0">
-                            <img src={img3} alt="" class="img-fluid" />
+                    <div className="col-lg-4 col-sm-6">
+                        <div className="service-img mt-4 mt-lg-0">
+                            <img src={img3} alt="" className="img-fluid" />
                         </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="service-content ps-4 mt-4 mt-lg-0">
+                    <div className="col-lg-4">
+                        <div className="service-content ps-4 mt-4 mt-lg-0">
                             <h2>Personal care <br />healthy living</h2>
-                            <p class="mt-4 mb-5 text-secondary form-text">We provide best leading medicle service Nulla perferendis veniam deleniti ipsum officia dolores repellat laudantium obcaecati neque.</p>
-                            <Link to={'/services'} className="btn-get-started scrollto">Services</Link>
+                            <p className="mt-4 mb-5 text-secondary form-text">We provide best leading medicle service Nulla perferendis veniam deleniti ipsum officia dolores repellat laudantium obcaecati neque.</p>
+                            <Link to={'/service'} className="btn-get-started scrollto">Services</Link>
                         </div>
                     </div>
                 </div>
