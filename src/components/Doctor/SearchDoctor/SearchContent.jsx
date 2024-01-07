7d334546a314ae04dd781e04af71a6559965804b
ujjalzaman <muzzwal97@gmail.com>
2024-01-07 23:32:18 +0600

    Updated Ui of doctors page

--- src/components/Doctor/SearchDoctor/SearchContent.jsx
+++ src/components/Doctor/SearchDoctor/SearchContent.jsx
@@ -4,73 +4,79 @@ import { Link } from 'react-router-dom';
 import showImg from '../../../images/specialities/specialities-01.png'
 import StarRatings from 'react-star-ratings';
 import { Tag } from 'antd';
+import './index.css';
+import { FaLocationArrow, FaRegThumbsUp, FaDollarSign, FaComment } from "react-icons/fa";
 
 const SearchContent = ({ data }) => {
     const services = data?.services?.split(',')
     return (
-        <div className="card mb-4">
-            <div className="card-body">
-                <div className='d-flex'>
-                    <div className='d-flex gap-3'>
-                        <div style={{maxWidth: 200, overflow: 'hidden' }}>
-                            <img src={img} className="img-fluid" alt="User Image" />
-                        </div>
-                        <div className="doc-info-cont">
-                            <h4><Link to={'/'} style={{ textDecoration: 'none' }}>Dr. {data?.firstName + ' ' + data?.lastName}</Link></h4>
-                            <p>{data?.designation}</p>
-                            <p className="doc-department"><img src={showImg} className="img-fluid" alt="Speciality" />Urology</p>
-
-                            <StarRatings
-                                rating={5}
-                                starRatedColor="blue"
-                                numberOfStars={5}
-                                name='rating'
-                                starDimension="20px"
-                            />
-                            <span className="d-inline-block">(4)</span>
-
-                            <div className="clinic-details">
-                                <p className="my-2"><i className="fas fa-map-marker-alt"></i> {data?.address}, {data?.country}</p>
-                                <ul className="clinic-gallery mt-3">
-                                    <li>
-                                        <img src={showImg} alt="Feature" style={{ maxWidth: "30px" }} />
-                                    </li>
-                                    <li>
-                                        <img src={showImg} alt="Feature" style={{ maxWidth: "30px" }} />
-                                    </li>
-                                    <li>
-                                        <img src={showImg} alt="Feature" style={{ maxWidth: "30px" }} />
-
-                                    </li>
-                                    <li>
-                                        <img src={showImg} alt="Feature" style={{ maxWidth: "30px" }} />
-                                    </li>
-                                </ul>
-                            </div>
-                            {
-                                services.map((item, id) => (
-                                    <Tag key={id + 51}>{item}</Tag>
-
-                                ))
-                            }
-                        </div>
+        <div className="mb-4 rounded" style={{ background: '#f3f3f3' }}>
+            <div className='d-flex p-3'>
+                <div className='d-flex gap-3'>
+                    <div className='doc-img-fluid d-flex align-items-center'>
+                        <img src={img} className="img-fluid" alt="User Image" />
                     </div>
-                    <div className="doc-info-right">
-                        <div className="clini-infos">
-                            <ul>
-                                <li><i className="far fa-thumbs-up"></i> 97%</li>
-                                <li><i className="far fa-comment"></i> 4 Feedback</li>
-                                <li><i className="fas fa-map-marker-alt"></i> Newyork, USA</li>
-                                <li><i className="far fa-money-bill-alt"></i> $150 - $250 <i className="fas fa-info-circle" data-toggle="tooltip" title="Lorem Ipsum"></i></li>
+                    <div className="doc-info">
+                        <h5 className='mb-0'><Link to={'/'}>Dr. {data?.firstName + ' ' + data?.lastName}</Link></h5>
+                        <p className='m-0 form-text'>{data?.designation}</p>
+                        <p className="doc-department m-0"><img src={showImg} className="img-fluid" alt="Speciality" />Urology</p>
+
+                        <div className='d-flex align-items-center'>
+                            <div>
+                                <StarRatings
+                                    rating={5}
+                                    starRatedColor="#f4c150"
+                                    numberOfStars={5}
+                                    name='rating'
+                                    starDimension="15px"
+                                    starSpacing="2px"
+                                />
+                            </div>
+                            <div>(4)</div>
+                        </div>
+
+                        <div className="clinic-details">
+                            <p className="form-text text-secondary"><FaLocationArrow /> {data?.address}, {data?.country}</p>
+                            <ul className="clinic-gallery mt-3">
+                                <li>
+                                    <img src={showImg} alt="Feature" style={{ maxWidth: "30px" }} />
+                                </li>
+                                <li>
+                                    <img src={showImg} alt="Feature" style={{ maxWidth: "30px" }} />
+                                </li>
+                                <li>
+                                    <img src={showImg} alt="Feature" style={{ maxWidth: "30px" }} />
+
+                                </li>
+                                <li>
+                                    <img src={showImg} alt="Feature" style={{ maxWidth: "30px" }} />
+                                </li>
                             </ul>
                         </div>
-                        <div className="clinic-booking">
-                            <Link to={'/doctors/profile'} className="view-pro-btn" style={{ textDecoration: 'none' }}>View Profile</Link>
-                            <Link to={`/booking/${data?.id}`} className="apt-btn" style={{ textDecoration: 'none' }}>Book Appointment</Link>
-                        </div>
+                        {
+                            services.map((item, id) => (
+                                <Tag key={id + 51}>{item}</Tag>
+
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className="doc-info-right">
+                    <div className="clini-infos">
+                        <ul>
+                            <li><FaRegThumbsUp />  97%</li>
+                            <li><FaComment /> 4 Feedback</li>
+                            <li><FaLocationArrow /> Newyork, USA</li>
+                            <li><FaDollarSign /> $150 - $250</li>
+                        </ul>
+                    </div>
+                    <div className="clinic-booking">
+                        <Link to={'/doctors/profile'} className="view-pro-btn">View Profile</Link>
+                        <Link to={`/booking/${data?.id}`} className="apt-btn">Book Appointment</Link>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
--- src/components/Doctor/SearchDoctor/SearchSidebar.jsx
+++ src/components/Doctor/SearchDoctor/SearchSidebar.jsx
@@ -57,45 +57,46 @@ const SearchSidebar = ({ setSearchTerm, setSorByGender, setSpecialist, setPriceR
     setSearchTerm(value);
   }
   return (
-    <div className="col-md-12 col-lg-4 col-xl-3 theiaStickySidebar">
+    <div className="col-md-12 col-lg-4 col-xl-3">
 
-      <div className="card search-filter shadow">
-        <div className="card-header bg-white">
-          <h4 className="card-title mb-0">Search Filter</h4>
+      <div className="p-3 rounded" style={{ background: '#f3f3f3' }}>
+        <h5 className='text-center mb-3' style={{ color: '#05335c' }}>Doctor Filter</h5>
+        <div className="mb-3">
+          <Search placeholder="Search..." onSearch={onSearch} enterButton allowClear />
         </div>
-        <div className="card-body">
-          <div className="mb-2">
-            <Search placeholder="Search..." onSearch={onSearch} enterButton allowClear />
-          </div>
-          <div className='mb-2'>
-            <DatePicker
-              style={{ width: "100%" }}
-              format="YYYY-MM-DD HH:mm:ss"
-              onChange={handleDateChange}
-            />
-          </div>
-          <div className='mb-2'>
-            <h5>Gender</h5>
-            <div className='d-flex flex-column'>
-              <Radio.Group options={options} onChange={onSelectGender} />
-            </div>
-          </div>
-          <div className='mb-2'>
-            <h5>Price Range</h5>
-            <Slider range defaultValue={[50, 100]} onChange={onRangeChange} />
-          </div>
 
-          <div>
-            <h5>Select Specialist</h5>
-            <div className='d-flex flex-column'>
-              <Radio.Group options={specialOptions} onChange={onSelectSepcialist} />
-            </div>
-          </div>
-          <Button className='w-100 mt-4 mb-2' type="primary" shape="round" icon={<FaSearch />} size="large">Search</Button>
-          {
-            Object.keys(query).length > 4 && <Button className='w-100 mt-4 mb-2' onClick={resetFilter} type="primary" shape="round" icon={<FaRedoAlt />} size="large">Reset</Button>
-          }
+        <div className='mb-3'>
+          <h6 style={{ color: '#05335c' }}>Date Range</h6>
+          <DatePicker
+            style={{ width: "100%" }}
+            format="YYYY-MM-DD HH:mm:ss"
+            onChange={handleDateChange}
+          />
         </div>
+
+        <div className='mb-3'>
+          <h6 style={{ color: '#05335c' }}>Gender</h6>
+          <div className='d-flex flex-column'>
+            <Radio.Group options={options} onChange={onSelectGender} />
+          </div>
+        </div>
+
+        <div className='mb-3'>
+          <h6 style={{ color: '#05335c' }}>Price Range</h6>
+          <Slider range defaultValue={[75, 150]} onChange={onRangeChange} />
+        </div>
+
+        <div className='mb-3'>
+          <h6 style={{ color: '#05335c' }}>Select Specialist</h6>
+          <div className='d-flex flex-column'>
+            <Radio.Group options={specialOptions} onChange={onSelectSepcialist} />
+          </div>
+        </div>
+
+        <Button className='w-100 mt-4 mb-2' type="primary" style={{backgroundColor:'#1977cc'}} shape="round" icon={<FaSearch />} size="sm">Search</Button>
+        {
+          Object.keys(query).length > 4 && <Button className='w-100 mt-4 mb-2' style={{backgroundColor:'#1977cc'}} onClick={resetFilter} type="primary" shape="round" icon={<FaRedoAlt />} size="sm">Reset</Button>
+        }
       </div>
 
     </div>
