cb6663a69a43b28716132913a484c1a51c2e2df6
ujjalzaman <muzzwal97@gmail.com>
2024-01-09 00:39:05 +0600

    Updated UI of footer and doctors profile

--- src/components/Doctor/DoctorProfile/OverView.jsx
+++ src/components/Doctor/DoctorProfile/OverView.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { FaBriefcase } from "react-icons/fa";
 
 const OverView = () => {
     return (
         <div className="col-md-12 col-lg-9">
-
             <div className='mb-3'>
                 <h5 className='overview-text'>About Me</h5>
                 <p className='text-secondary'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -12,118 +14,138 @@ const OverView = () => {
             <div>
                 <h5 className='overview-text'>Education</h5>
 
-                <div className="experience-box">
-                    <ul className="experience-list">
-                        <li>
-                            <div className="experience-user">
-                                <div className="before-circle"></div>
-                            </div>
-                            <div className="experience-content">
-                                <div className="timeline-content">
-                                    <a href="#/" className="name">American Dental Medical University</a>
-                                    <div>BDS</div>
-                                    <span className="time">1998 - 2003</span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="experience-user">
-                                <div className="before-circle"></div>
-                            </div>
-                            <div className="experience-content">
-                                <div className="timeline-content">
-                                    <a href="#/" className="name">American Dental Medical University</a>
-                                    <div>MDS</div>
-                                    <span className="time">2003 - 2005</span>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+                <VerticalTimeline>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#2e81c4', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #2e81c4' }}
+                        date="2011 - 2000"
+                        iconStyle={{ background: '#2e81c4', color: '#fff' }}
+                        icon={<FaBriefcase />}
+                    >
+                        <h5 className="text-white">American Dental Medical University</h5>
+                        <h6 className="text-white">Miami, FL</h6>
+                        <p style={{ fontSize: '14px' }}>
+                            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                        </p>
+                    </VerticalTimelineElement>
+
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#2e81c4', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #2e81c4' }}
+                        date="2003 - 2005"
+                        iconStyle={{ background: '#2e81c4', color: '#fff' }}
+                        icon={<FaBriefcase />}
+                    >
+                        <h5 className="text-white">American Dental Medical University</h5>
+                        <h6 className="text-white">Miami, FL</h6>
+                        <p style={{ fontSize: '14px' }}>
+                            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                        </p>
+                    </VerticalTimelineElement>
+
+                </VerticalTimeline>
+
             </div>
-            <div>
+            <div className='my-5'>
                 <h5 className='overview-text'>Work & Experience</h5>
-                <div className="experience-box">
-                    <ul className="experience-list">
-                        <li>
-                            <div className="experience-user">
-                                <div className="before-circle"></div>
-                            </div>
-                            <div className="experience-content">
-                                <div className="timeline-content">
-                                    <a href="#/" className="name">Glowing Smiles Family Dental Clinic</a>
-                                    <span className="time">2010 - Present (5 years)</span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="experience-user">
-                                <div className="before-circle"></div>
-                            </div>
-                            <div className="experience-content">
-                                <div className="timeline-content">
-                                    <a href="#/" className="name">Comfort Care Dental Clinic</a>
-                                    <span className="time">2007 - 2010 (3 years)</span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="experience-user">
-                                <div className="before-circle"></div>
-                            </div>
-                            <div className="experience-content">
-                                <div className="timeline-content">
-                                    <a href="#/" className="name">Dream Smile Dental Practice</a>
-                                    <span className="time">2005 - 2007 (2 years)</span>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+
+                <VerticalTimeline>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#2e81c4', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #2e81c4' }}
+                        date="2010 - Present (5 years)"
+                        iconStyle={{ background: '#2e81c4', color: '#fff' }}
+                        icon={<FaBriefcase />}
+                    >
+                        <h5 className="text-white">Glowing Smiles Family Dental Clinic</h5>
+                        <h6 className="text-white">Miami, FL</h6>
+                        <p style={{ fontSize: '14px' }}>
+                            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                        </p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#2e81c4', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #2e81c4' }}
+                        date="2007 - 2010 (3 years)"
+                        iconStyle={{ background: '#2e81c4', color: '#fff' }}
+                        icon={<FaBriefcase />}
+                    >
+                        <h5 className="text-white">Comfort Care Dental Clinic</h5>
+                        <h6 className="text-white">Miami, FL</h6>
+                        <p style={{ fontSize: '14px' }}>
+                            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                        </p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#2e81c4', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #2e81c4' }}
+                        date="2005 - 2007 (2 years)"
+                        iconStyle={{ background: '#2e81c4', color: '#fff' }}
+                        icon={<FaBriefcase />}
+                    >
+                        <h5 className="text-white">Dream Smile Dental Practice</h5>
+                        <h6 className="text-white">Miami, FL</h6>
+                        <p style={{ fontSize: '14px' }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dignissimos.</p>
+                    </VerticalTimelineElement>
+
+
+                  
+
+                </VerticalTimeline>
+                
             </div>
             <div >
                 <h5 className='overview-text'>Awards</h5>
 
-                <div className="experience-box">
-                    <ul className="experience-list">
-                        <li>
-                            <div className="experience-user">
-                                <div className="before-circle"></div>
-                            </div>
-                            <div className="experience-content">
-                                <div className="timeline-content">
-                                    <p className="exp-year">July 2019</p>
-                                    <h6 className="exp-title">Humanitarian Award</h6>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a ipsum tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="experience-user">
-                                <div className="before-circle"></div>
-                            </div>
-                            <div className="experience-content">
-                                <div className="timeline-content">
-                                    <p className="exp-year">March 2011</p>
-                                    <h6 className="exp-title">Certificate for International Volunteer Service</h6>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a ipsum tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="experience-user">
-                                <div className="before-circle"></div>
-                            </div>
-                            <div className="experience-content">
-                                <div className="timeline-content">
-                                    <p className="exp-year">May 2008</p>
-                                    <h4 className="exp-title">The Dental Professional of The Year Award</h4>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a ipsum tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+                <VerticalTimeline>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#2e81c4', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #2e81c4' }}
+                        date="July 2019"
+                        iconStyle={{ background: '#2e81c4', color: '#fff' }}
+                        icon={<FaBriefcase />}
+                    >
+                        <h5 className="text-white">Humanitarian Award</h5>
+                        <h6 className="text-white">Miami, FL</h6>
+                        <p style={{ fontSize: '14px' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a ipsum tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#2e81c4', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #2e81c4' }}
+                        date="March 2011"
+                        iconStyle={{ background: '#2e81c4', color: '#fff' }}
+                        icon={<FaBriefcase />}
+                    >
+                        <h5 className="text-white">Certificate for International Volunteer Service</h5>
+                        <h6 className="text-white">Miami, FL</h6>
+                        <p style={{ fontSize: '14px' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a ipsum tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#2e81c4', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #2e81c4' }}
+                        date="March 2011"
+                        iconStyle={{ background: '#2e81c4', color: '#fff' }}
+                        icon={<FaBriefcase />}
+                    >
+                        <h5 className="text-white">The Dental Professional of The Year Award</h5>
+                        <h6 className="text-white">Miami, FL</h6>
+                        <p style={{ fontSize: '14px' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a ipsum tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                    </VerticalTimelineElement>
+
+                </VerticalTimeline>
             </div>
             <div>
                 <h5 className='overview-text'>Services</h5>
@@ -147,9 +169,7 @@ const OverView = () => {
                     <li>Prosthodontics</li>
                 </ul>
             </div>
-
         </div>
     )
 }
-
-export default OverView
+export default OverView;
--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Footer.css';
 import logo from '../../../images/logo.png';
+import { Link } from 'react-router-dom';
+import { FaAngleDoubleRight } from "react-icons/fa";
 
 const Footer = () => {
 	return (
@@ -11,10 +13,12 @@ const Footer = () => {
 						<div className="col-lg-3 col-md-6">
 							<div className="footer-widget footer-about">
 								<div className="footer-logo">
-									<img src={logo} alt="logo" style={{maxWidth:'160px'}}/>
+									<Link to={'/'}>
+										<img src={logo} alt="logo" style={{ maxWidth: '160px' }} />
+									</Link>
 								</div>
 								<div className="footer-about-content">
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<p className='form-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 								</div>
 							</div>
 						</div>
@@ -23,11 +27,11 @@ const Footer = () => {
 							<div className="footer-widget footer-menu">
 								<h2 className="footer-title">For Patients</h2>
 								<ul>
-									<li><a href="#"><i className="fas fa-angle-double-right"></i> Search for Doctors</a></li>
-									<li><a href="#"><i className="fas fa-angle-double-right"></i> Login</a></li>
-									<li><a href="#"><i className="fas fa-angle-double-right"></i> Register</a></li>
-									<li><a href="#"><i className="fas fa-angle-double-right"></i> Booking</a></li>
-									<li><a href="#"><i className="fas fa-angle-double-right"></i> Patient Dashboard</a></li>
+									<li><Link to={'/doctors'}><FaAngleDoubleRight className='icon' />  Search for Doctors</Link></li>
+									<li><Link to={'/login'}><FaAngleDoubleRight className='icon' />  Login</Link></li>
+									<li><Link to={'/login'}><FaAngleDoubleRight className='icon' />  Register</Link></li>
+									<li><Link to={'/doctors'}><FaAngleDoubleRight className='icon' />  Booking</Link></li>
+									<li><Link to={'/'}><FaAngleDoubleRight className='icon' />  Patient Dashboard</Link></li>
 								</ul>
 							</div>
 						</div>
@@ -37,11 +41,10 @@ const Footer = () => {
 							<div className="footer-widget footer-menu">
 								<h2 className="footer-title">For Doctors</h2>
 								<ul>
-									<li><a href="#"><i className="fas fa-angle-double-right"></i> Appointments</a></li>
-									<li><a href="#"><i className="fas fa-angle-double-right"></i> Chat</a></li>
-									<li><a href="#"><i className="fas fa-angle-double-right"></i> Login</a></li>
-									<li><a href="#"><i className="fas fa-angle-double-right"></i> Register</a></li>
-									<li><a href="#"><i className="fas fa-angle-double-right"></i> Doctor Dashboard</a></li>
+									<li><Link to={'/'}><FaAngleDoubleRight className='icon' /> Appointments</Link></li>
+									<li><Link to={'/login'}><FaAngleDoubleRight className='icon' /> Login</Link></li>
+									<li><Link to={'/register'}><FaAngleDoubleRight className='icon' /> Register</Link></li>
+									<li><Link to={'/dashboard'}><FaAngleDoubleRight className='icon' /> Doctor Dashboard</Link></li>
 								</ul>
 							</div>
 						</div>
@@ -52,11 +55,11 @@ const Footer = () => {
 								<div className="footer-contact-info">
 									<div className="footer-address">
 										<span><i className="fas fa-map-marker-alt"></i></span>
-										<p> 3556  Beech Street, San Francisco,<br /> California, CA 94108 </p>
+										<p> 121, Mirzapure Union office,<br /> Sylhet, Bangladesh 03214 </p>
 									</div>
 									<p>
 										<i className="fas fa-phone-alt"></i>
-										+1 315 369 5943
+										+88 017 51 040425
 									</p>
 									<p className="mb-0">
 										<i className="fas fa-envelope"></i>
@@ -84,8 +87,8 @@ const Footer = () => {
 							<div className="col-md-6 col-lg-6">
 								<div className="copyright-menu">
 									<ul className="policy-menu d-flex gap-2">
-										<a href="#" className='text-white'>Terms and Conditions</a>
-										<a href="#" className='text-white'>Policy</a>
+										<Link to={'/'} className='text-white'>Terms and Conditions</Link>
+										<Link to={'/'} className='text-white'>Policy</Link>
 									</ul>
 								</div>
 							</div>
