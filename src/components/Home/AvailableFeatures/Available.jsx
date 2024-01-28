929b54d08af7ac8789aad154fd0847608e8beacf
ujjalzaman <muzzwal97@gmail.com>
2024-01-28 12:37:58 +0600

    Updated availabe feature

--- src/components/Home/AvailableFeatures/Available.jsx
+++ src/components/Home/AvailableFeatures/Available.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import img from '../../../images/features/feature.png';
 import img2 from '../../../images/features/feature-02.jpg';
-import img3 from '../../../images/features/feature-02.jpg';
-import img4 from '../../../images/features/feature-03.jpg';
+import img3 from '../../../images/features/feature-01.jpg';
+import img4 from '../../../images/features/feature-05.jpg';
+import img5 from '../../../images/features/feature-06.jpg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay } from 'swiper/modules';
 import './index.css';
 
 const Availabe = () => {
+	const availabeServiceArray = [
+		{ title: 'ICU', img: img },
+		{ title: 'Chamber', img: img5 },
+		{ title: 'Patient Ward', img: img2 },
+		{ title: 'Test Room', img: img3 },
+		{ title: 'Laboratory', img: img4 },
+	]
 	return (
 		<section className="container section-features">
 			<div className="container-fluid">
@@ -20,25 +30,25 @@ const Availabe = () => {
 						</div>
 						<div className="d-flex justify-content-center align-items-center gap-4">
 
-							<div className="feature-item text-center">
-								<img src={img4} className="img-fluid" alt="" />
-								<p>ICU</p>
-							</div>
-
-							<div className="feature-item text-center">
-								<img src={img4} className="img-fluid" alt="" />
-								<p>Patient Ward</p>
-							</div>
-
-							<div className="feature-item text-center">
-								<img src={img2} className="img-fluid" alt="Feature" />
-								<p>Test Room</p>
-							</div>
-
-							<div className="feature-item text-center">
-								<img src={img3} className="img-fluid" alt="" />
-								<p>Laboratory</p>
-							</div>
+							<Swiper
+								spaceBetween={2}
+								slidesPerView={4}
+								modules={[Navigation, Autoplay]}
+								loop={true}
+								centeredSlides={true}
+								autoplay={{ delay: 2000, disableOnInteraction: false }}
+							>
+								{
+									availabeServiceArray.map((item) => (
+										<SwiperSlide key={item.title} className='my-2'>
+											<div className="feature-item text-center">
+												<img src={item.img} className="img-fluid" alt="" />
+												<p>{item.title}</p>
+											</div>
+										</SwiperSlide>
+									))
+								}
+							</Swiper>
 						</div>
 					</div>
 				</div>
