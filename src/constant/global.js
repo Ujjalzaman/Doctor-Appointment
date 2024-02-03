0f8db52ded97c6aaecb25043d57f62b420cad542
ujjalzaman <muzzwal97@gmail.com>
2024-02-03 12:12:03 +0600

    Implemented All the required functionality for handle treatment form from client side

--- src/constant/global.js
+++ src/constant/global.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 export const bloodGrup = ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']
 export const bloodGrupOptions = bloodGrup.map((data) => {
     return {
@@ -71,8 +72,276 @@ const appointmentStatus = [
 
 export const appointemntStatusOption = appointmentStatus.map((item) => {
     return {
-        lebel: item,
+        label: item,
         value: item
     }
 })
 
+//Daignosis
+
+const medical_diagnoses = [
+    "Hypertension",
+    "Type 2 Diabetes Mellitus",
+    "Coronary Artery Disease",
+    "Osteoarthritis",
+    "Rheumatoid Arthritis",
+    "Chronic Obstructive Pulmonary Disease (COPD)",
+    "Asthma",
+    "Chronic Kidney Disease",
+    "Migraine",
+    "Major Depressive Disorder",
+    "Generalized Anxiety Disorder",
+    "Schizophrenia",
+    "Bipolar Disorder",
+    "Attention-Deficit/Hyperactivity Disorder (ADHD)",
+    "Thyroid Disorders (e.g., Hypothyroidism, Hyperthyroidism)",
+    "Gastroesophageal Reflux Disease (GERD)",
+    "Peptic Ulcer Disease",
+    "Irritable Bowel Syndrome (IBS)",
+    "Crohn's Disease",
+    "Ulcerative Colitis",
+    "Chronic Hepatitis",
+    "Cirrhosis of the Liver",
+    "Stroke",
+    "Epilepsy",
+    "Chronic Heart Failure"
+]
+const medical_diseases = [
+    "Hypertension",
+    "Type 2 Diabetes Mellitus",
+    "Coronary Artery Disease",
+    "Osteoarthritis",
+    "Rheumatoid Arthritis",
+    "Chronic Obstructive Pulmonary Disease (COPD)",
+    "Asthma",
+    "Chronic Kidney Disease",
+    "Migraine",
+    "Major Depressive Disorder",
+    "Generalized Anxiety Disorder",
+    "Schizophrenia",
+    "Bipolar Disorder",
+    "Attention-Deficit/Hyperactivity Disorder (ADHD)",
+    "Thyroid Disorders (e.g., Hypothyroidism, Hyperthyroidism)",
+    "Gastroesophageal Reflux Disease (GERD)",
+    "Peptic Ulcer Disease",
+    "Irritable Bowel Syndrome (IBS)",
+    "Crohn's Disease",
+    "Ulcerative Colitis",
+    "Chronic Hepatitis",
+    "Cirrhosis of the Liver",
+    "Stroke",
+    "Epilepsy",
+    "Chronic Heart Failure",
+    "Obesity",
+    "Arthritis",
+    "Dementia",
+    "Psoriasis",
+    "Celiac Disease"
+];
+
+const bronchitisList = [
+    "Acute Bronchitis",
+    "Chronic Bronchitis",
+    "Infectious Bronchitis",
+    "Viral Bronchitis",
+    "Bacterial Bronchitis",
+    "Allergic Bronchitis",
+    "Non-infectious Bronchitis",
+    "Smoker's Cough",
+    "Chest Cold",
+    "Respiratory Infection",
+    "COPD Exacerbation",
+    "Productive Cough",
+    "Wheezing",
+    "Shortness of Breath",
+    "Chest Discomfort",
+    "Mucous Production",
+    "Respiratory Distress",
+    "Bronchospasm",
+    "Airway Inflammation",
+    "Persistent Cough",
+    "Throat Irritation",
+    "Chest Congestion",
+    "Fever",
+    "Fatigue",
+    "Malaise",
+    "Breathing Difficulty",
+    "Green or Yellow Sputum",
+    "Rhonchi",
+    "Crackles"
+];
+
+const medicalCheckupList = [
+    "Physical Examination",
+    "Blood Pressure Measurement",
+    "Blood Tests",
+    "Cholesterol Panel",
+    "Blood Glucose Test",
+    "Complete Blood Count (CBC)",
+    "Thyroid Function Tests",
+    "Liver Function Tests",
+    "Kidney Function Tests",
+    "Urinalysis",
+    "Body Mass Index (BMI) Measurement",
+    "Vision Test",
+    "Hearing Test",
+    "Dental Checkup",
+    "Skin Examination",
+    "Cancer Screenings (e.g., Mammogram, Pap Smear, Prostate Specific Antigen)",
+    "Bone Density Test",
+    "Electrocardiogram (ECG or EKG)",
+    "Chest X-ray",
+    "Pulmonary Function Tests",
+    "Colonoscopy",
+    "Stool Test for Colorectal Cancer",
+    "DEXA Scan (Dual-Energy X-ray Absorptiometry)",
+    "HIV Test",
+    "Sexually Transmitted Infections (STI) Screenings",
+    "Immunizations and Vaccinations",
+    "Eye Exam",
+    "Psychological Assessment",
+    "Annual Checkup with General Practitioner"
+];
+
+const dosageList = [
+    "250 mg",
+    "1000 mg",
+    "75 mg",
+    "50 mg",
+    "5 mg",
+    "150 mg",
+    "300 mg",
+    "200 mg",
+    "10 mg",
+    "20 mg"
+];
+const frequenciesList = [
+    "Once Daily (QD)",
+    "Twice Daily (BID)",
+    "Three Times Daily (TID)",
+    "Four Times Daily (QID)",
+    "Every 6 Hours (Q6H)",
+    "As Needed (PRN)",
+    "Once Every Other Day (QOD)",
+    "Every 8 Hours (Q8H)",
+    "Every 12 Hours (Q12H)",
+    "Once Weekly (QW)"
+];
+
+const patientStatusList = [
+    "Stable",
+    "Critical",
+    "Serious",
+    "Guarded",
+    "Unstable",
+    "Recovering",
+    "Critical but Stable",
+    "Comfort Measures Only (CMO)",
+    "Acute",
+    "Chronic"
+];
+
+export const PatientStatus = patientStatusList.map((item) => {
+    return {
+        label: item,
+        value: item
+    }
+})
+
+
+
+export const DiagnosisOptions = medical_diagnoses.map((item) => {
+    return { label: item, value: item }
+})
+
+export const DiseaseOptions = medical_diseases.map((item) => {
+    return { label: item, value: item }
+})
+
+export const BronchitisOptions = bronchitisList.map((item) => {
+    return { label: item, value: item }
+})
+
+export const MedicalCheckupOptions = medicalCheckupList.map((item) => {
+    return { label: item, value: item }
+})
+
+export const DosageOptions = dosageList.map((item) => {
+    return {
+        label: item,
+        value: item
+    }
+})
+
+export const FrequencyOptions = frequenciesList.map((item) => {
+    return {
+        label: item,
+        value: item
+    }
+})
+
+export const DateRangePresets = [
+    {
+        label: 'Next 3 Days',
+        value: [dayjs(), dayjs().add(3, 'd')],
+    },
+    {
+        label: 'Next 7 Days',
+        value: [dayjs(), dayjs().add(7, 'd')],
+    },
+    {
+        label: 'Next 10 Days',
+        value: [dayjs(), dayjs().add(10, 'd')],
+    },
+    {
+        label: 'Next 14 Days',
+        value: [dayjs(), dayjs().add(14, 'd')],
+    },
+    {
+        label: 'Next 30 Days',
+        value: [dayjs(), dayjs().add(30, 'd')],
+    },
+    {
+        label: 'Next 2 Month',
+        value: [dayjs(), dayjs().add(60, 'd')],
+    },
+    {
+        label: 'Next 3 Month',
+        value: [dayjs(), dayjs().add(90, 'd')],
+    },
+];
+
+export const DatePickerSinglePresets = [
+    {
+        label: 'Tomorrow',
+        value: dayjs().add(1, 'd'),
+    },
+    {
+        label: 'Next 3 Day',
+        value: dayjs().add(3, 'd'),
+    },
+    {
+        label: 'Next Week',
+        value: dayjs().add(7, 'd'),
+    },
+    {
+        label: 'Next 2 Week',
+        value: dayjs().add(15, 'd'),
+    },
+    {
+        label: 'Next Month',
+        value: dayjs().add(30, 'd'),
+    },
+    {
+        label: 'Next 2 Month',
+        value: dayjs().add(60, 'd'),
+    },
+    {
+        label: 'Next 3 Month',
+        value: dayjs().add(90, 'd'),
+    },
+    {
+        label: 'Next 6 Month',
+        value: dayjs().add(180, 'd'),
+    },
+]
