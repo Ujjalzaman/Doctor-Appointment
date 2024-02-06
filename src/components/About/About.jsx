f2a5ce9f015d77b70cd82c5b3eaab0babe962cc5
ujjalzaman <muzzwal97@gmail.com>
2024-02-06 19:11:41 +0600

    Updated About us page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,15 +2,68 @@ import React from 'react'
 import './index.css';
 import Header from '../Shared/Header/Header';
 import Footer from '../Shared/Footer/Footer';
-import img2 from '../../images/doc/doctor 5.jpg'
-import docImg from '../../images/doc/doctor 3.jpg'
+import ImageHeading from '../../images/doc/doctor 5.jpg'
+import img from '../../images/logo.png'
 import SubHeader from '../Shared/SubHeader';
+import { useGetAllBlogsQuery } from '../../redux/api/blogApi';
+import { Empty, message } from 'antd';
+import { Link } from 'react-router-dom';
+import { truncate } from '../../utils/truncate';
+import { useGetDoctorsQuery } from '../../redux/api/doctorApi';
 
 const About = () => {
+    const { data, isError, isLoading } = useGetAllBlogsQuery({ limit: 4 });
+    const { data: doctorData, isLoading: DoctorIsLoading, isError: doctorIsError } = useGetDoctorsQuery({ limit: 4 });
+
+    const blogData = data?.blogs;
+    const doctors = doctorData?.doctors;
+
+    let doctorContent = null;
+    if (!DoctorIsLoading && doctorIsError) doctorContent = <div>Something Went Wrong !</div>
+    if (!DoctorIsLoading && !doctorIsError && doctors?.length === 0) doctorContent = <div><Empty /></div>
+    if (!DoctorIsLoading && !doctorIsError && doctors?.length > 0) doctorContent =
+        <>
+            {doctors && doctors.map((item, id) => (
+                <div className="col-lg-3 col-md-6 col-sm-6" key={id + item.id}>
+                    <div className="card shadow border-0 mb-5 mb-lg-0">
+                        {item.img && <img src={item.img} class="img-fluid w-100" alt="" />}
+                        <div className="p-2">
+                            <h4 className="mt-4 mb-0" style={{ color: '#223a66' }}><a>{item?.firstName + ' ' + item?.lastName}</a></h4>
+                            <p>{item?.designation}</p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </>
+
+    let content = null;
+    if (!isLoading && isError) content = <div>{message.error('Something went Wrong!')}</div>
+    if (!isLoading && !isError && blogData?.length === 0) content = <Empty />
+    if (!isLoading && !isError && blogData?.length > 0) content =
+        <>
+            {
+                blogData && blogData?.map((item, id) => (
+                    <div className="col-lg-3 col-md-6" key={id + item.id}>
+                        <div className="card shadow border-0 mb-5 mb-lg-0">
+                            <img src={item?.img} alt="blog Image" width={300} height={200} className="w-100  rounded-top image-hover" style={{ objectFit: 'contain' }} />
+
+                            <div className='p-2'>
+                                <Link to={`/blog/${item?.id}`}>
+                                    <h6 className="text-start mb-1 text-capitalize" style={{ color: '#223a66' }}>{truncate(item?.title, 40)}</h6>
+                                </Link>
+                                <div className="px-2">
+                                    <p className="form-text text-start text-capitalize">{truncate(item?.description, 80)}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+        </>
     return (
         <>
             <Header />
-            <SubHeader title="about us" subtitle="Lorem ipsum dolor sit amet consectetur adipisicing."/>
+            <SubHeader title="about us" subtitle="Lorem ipsum dolor sit amet consectetur adipisicing." />
             <div className="container" style={{ marginBottom: 100, marginTop: 100 }}>
                 <div className="row p-5">
                     <div className="col-lg-4">
@@ -18,33 +71,21 @@ const About = () => {
                             <h2 className='text-uppercase'>Our Doctors Acheivement</h2>
                             <p className='form-text m-0'>Lorem ipsum dolor sit amet.</p>
                         </div>
+                        <p className='mt-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, quod laborum alias. Vitae dolorum, officia sit! Saepe ullam facere at, consequatur incidunt, quae esse, quis ut reprehenderit dignissimos, libero delectus.</p>
                     </div>
 
                     <div className="col-lg-8">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, quod laborum alias. Vitae dolorum, officia sit! Saepe ullam facere at, consequatur incidunt, quae esse, quis ut reprehenderit dignissimos, libero delectus.</p>
-                        <img src={img2} alt="" className="img-fluid rounded shadow" />
+
+                        <img src={ImageHeading} alt="" className="img-fluid rounded shadow" />
                     </div>
                 </div>
             </div>
 
             <div className="container" style={{ marginBottom: 100, marginTop: 100 }}>
                 <div className="row">
-                    {
-                        Array(4).fill(null).map((_item, id) => (
-                            <div className="col-lg-3 col-md-6" key={id + 4}>
-                                <div className="card shadow border-0 mb-5 mb-lg-0">
-                                    <img src={img2} alt="" className="m-0 img-fluid w-100" />
-                                    <div className='p-2'>
-                                        <h4 className="mt-3" style={{ color: '#223a66' }}>Healthcare for Kids</h4>
-                                        <p className='form-text'>Voluptate aperiam esse possimus maxime repellendus, nihil quod accusantium .</p>
-                                    </div>
-                                </div>
-                            </div>
-                        ))
-                    }
+                    {content}
                 </div>
             </div>
-
 
             <div className="container" style={{ marginBottom: 100, marginTop: 100 }}>
                 <div className="row align-items-center">
@@ -60,7 +101,7 @@ const About = () => {
                                 Array(6).fill(null).map((_, id) => (
                                     <div className="col-lg-4 col-md-6 col-sm-6" key={id + 3}>
                                         <div className="award-img">
-                                            <img src={docImg} alt="" className="img-fluid" />
+                                            <img src={img} alt="" className="img-fluid" />
                                         </div>
                                     </div>
                                 ))
@@ -81,24 +122,10 @@ const About = () => {
                 </div>
 
                 <div className="row">
-                    {
-                        Array(4).fill(null).map((_, id) => (
-                            <div className="col-lg-3 col-md-6 col-sm-6" key={id + 2}>
-                                <div className="card shadow border-0 mb-5 mb-lg-0">
-                                    <img src={docImg} alt="" className="img-fluid w-100" />
-
-                                    <div className="p-2">
-                                        <h4 className="mt-4 mb-0" style={{ color: '#223a66' }}><a>John Marshal</a></h4>
-                                        <p>Internist, Emergency Physician</p>
-                                    </div>
-                                </div>
-                            </div>
-                        ))
-                    }
+                    {doctorContent}
 
                 </div>
             </div>
-
 
             <div className="container say-about" style={{ marginBottom: 100, marginTop: 100 }}>
                 <div className="row">
