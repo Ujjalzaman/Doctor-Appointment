760a9c68b1aff712b174f744561bf559404d2069
ujjalzaman <muzzwal97@gmail.com>
2024-02-08 19:26:24 +0600

    Implemented Email sent facility in contact us page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Footer from '../Shared/Footer/Footer'
 import { useForm } from 'react-hook-form';
 import { FaLocationArrow, FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 import Header from '../Shared/Header/Header';
 import './index.css';
 import SubHeader from '../Shared/SubHeader';
+import { useContactMutation } from '../../redux/api/contactApi';
+import { message } from 'antd';
 
 const Contact = () => {
-    const { register, handleSubmit } = useForm({});
+    const [contact, {isLoading, isError, error, isSuccess}]= useContactMutation();
+    const { register, handleSubmit, reset } = useForm({});
     const onSubmit = (data) => {
-
+        contact(data);
+        reset();
     };
+    
+    useEffect(() => {
+        if(isSuccess){
+            message.success("Successfully Message Send !");
+        }
+        if(isError && error){
+            message.error(error?.data?.message);
+        }
+    }, [])
     return (
         <>
             <Header />
@@ -18,7 +31,7 @@ const Contact = () => {
             <section id="contact" className="contact mt-5 mb-5">
                 <div>
                     <div className="container">
-                    {/* eslint-disable-next-line */}
+                        {/* eslint-disable-next-line */}
                         <iframe style={{ border: 0, width: "100%", height: "350px" }} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
                     </div>
                 </div>
@@ -61,40 +74,40 @@ const Contact = () => {
                                     <div className="col-md-6">
                                         <div className="form-group mb-2 card-label">
                                             <label>First Name</label>
-                                            <input type="text" name="" id="" placeholder="First Name" className="form-control mb-3" />
+                                            <input required {...register("firstName")} className="form-control" placeholder='First Name'/>
                                         </div>
                                     </div>
 
                                     <div className="col-md-6">
                                         <div className="form-group mb-2 card-label">
                                             <label>Last Name</label>
-                                            <input type="text" name="" id="" placeholder="Last Name" className="form-control mb-3" />
+                                            <input required {...register("lastName")} className="form-control" placeholder='Last Name'/>
                                         </div>
                                     </div>
 
                                     <div className="col-md-12">
                                         <div className="form-group mb-2 card-label">
                                             <label>Email</label>
-                                            <input type="email" name="" id="" placeholder="Email" className="form-control mb-3" />
+                                            <input required {...register("email")} type='email' className="form-control" placeholder="Email" />
                                         </div>
                                     </div>
 
                                     <div className="col-md-12">
                                         <div className="form-group mb-2 card-label">
                                             <label>Subject</label>
-                                            <input type="text" name="" id="" placeholder="enter your subject" className="form-control mb-3" />
+                                            <input required {...register("subject")} className="form-control" placeholder="Enter your subject"/>
                                         </div>
                                     </div>
 
                                     <div className="col-md-12">
                                         <div className="form-group">
-                                            <label className='form-label'>Subject</label>
-                                            <textarea name="" id="" cols="30" rows="10" placeholder="enter your message" className="form-control mb-3" />
+                                            <label className='form-label'>Message</label>
+                                            <textarea required {...register("text")} className="form-control mb-3" cols="30" rows="10" placeholder="enter your message"/>
                                         </div>
                                     </div>
 
                                     <div className="text-center mt-3 mb-5">
-                                        <button type='submit' className="appointment-btn">Send Message</button>
+                                        <button disabled={isLoading} type='submit' className="appointment-btn">Send Message</button>
                                     </div>
                                 </form>
                             </div>
