16c67a04aa13d7e4db36d7a8422976a401d7249e
ujjalzaman <muzzwal97@gmail.com>
2024-02-13 00:58:36 +0600

    Implemented Email Verfication System

--- api/src/app/modules/auth/auth.controller.ts
+++ api/src/app/modules/auth/auth.controller.ts
@@ -3,6 +3,11 @@ import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import { AuthService } from "./auth.service";
 import config from "../../../config";
+import path from 'path';
+import prisma from "../../../shared/prisma";
+import ApiError from "../../../errors/apiError";
+import httpStatus from "http-status";
+import moment from "moment";
 
 const Login = catchAsync(async (req: Request, res: Response) => {
     const result = await AuthService.loginUser(req.body);
@@ -21,6 +26,55 @@ const Login = catchAsync(async (req: Request, res: Response) => {
     })
 })
 
+const VerifyUser = catchAsync(async (req: Request, res: Response) => {
+    try {
+        const { userId } = req.params;
+        const isUserExist = await prisma.doctor.findUnique({
+            where: {
+                id: userId
+            }
+        })
+        if (!isUserExist) {
+            throw new ApiError(httpStatus.NOT_FOUND, "User is not found !!");
+        }
+        const getVerficationUser = await prisma.userVerfication.findFirst({
+            where: {
+                userId: userId
+            }
+        })
+        if (getVerficationUser) {
+            const expiresAt = moment(getVerficationUser.expiresAt);
+            const currentTime = moment();
+            if (expiresAt.isBefore(currentTime)) {
+                await prisma.doctor.update({
+                    where: {
+                        id: isUserExist.id
+                    },
+                    data: {
+                        verified: true
+                    }
+                })
+                res.redirect('/api/v1/auth/expired/link');
+            } else {
+                res.redirect('/api/v1/auth/verified');
+            }
+        }
+    } catch (error) {
+        throw new ApiError(httpStatus.NOT_FOUND, "Internal Server Error");
+    }
+})
+
+const Verified = catchAsync(async (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../../../../template/verfied.html"))
+})
+
+const VerficationExpired = catchAsync(async (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../../../../template/expiredVarification.html"))
+})
+
 export const AuthController = {
-    Login
+    Login,
+    VerifyUser,
+    Verified,
+    VerficationExpired
 }
--- api/src/app/modules/auth/auth.service.ts
+++ api/src/app/modules/auth/auth.service.ts
@@ -34,6 +34,30 @@ const loginUser = async (user: any): Promise<ILginResponse> => {
     return { accessToken, user: { role, userId } }
 }
 
+const VerificationUser = async (user: any): Promise<ILginResponse> => {
+    const { email: IEmail, password } = user;
+    const isUserExist = await prisma.auth.findUnique({
+        where: { email: IEmail }
+    })
+
+    if (!isUserExist) {
+        throw new ApiError(httpStatus.NOT_FOUND, "User is not Exist !");
+    }
+    const isPasswordMatched = await bcrypt.compare(password, isUserExist.password);
+
+    if (!isPasswordMatched) {
+        throw new ApiError(httpStatus.NOT_FOUND, "Password is not Matched !");
+    }
+    const { email, id, role, userId } = isUserExist;
+    const accessToken = JwtHelper.createToken(
+        { role, userId },
+        config.jwt.secret as Secret,
+        config.jwt.JWT_EXPIRES_IN as string
+    )
+    return { accessToken, user: { role, userId } }
+}
+
 export const AuthService = {
-    loginUser
+    loginUser,
+    VerificationUser
 }
