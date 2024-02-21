4faae7da98ed948a89201619e65b66357d2650f8
ujjalzaman <muzzwal97@gmail.com>
2024-02-21 19:07:27 +0600

    fixed bugs for deployment

--- api/dist/app/modules/blog/blog.interface.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.blogSearchablFields = void 0;
-exports.blogSearchablFields = ['title', 'description'];
--- api/dist/constants/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IOptions = exports.IAuthRules = exports.patientCondition = exports.BloodGroup = void 0;
-exports.BloodGroup = ['O+', 'O-', 'B+', 'B-', 'AB+', 'AB-', 'A+', 'A-'];
-exports.patientCondition = ["Critical", "Serious", "Stable", "Acute", "Chronic", "Critical but Stable", "Non-Critical", "Terminal", "Emergent", "Urgent"];
-exports.IAuthRules = ['ADMIN', "PATIENT", "DOCTOR"];
-exports.IOptions = ['limit', 'page', 'sortBy', 'sortOrder'];
