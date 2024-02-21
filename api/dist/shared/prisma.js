6fdef6b4e948eb9c2dbc694997121ade56e7cd13
ujjalzaman <muzzwal97@gmail.com>
2024-02-21 19:07:27 +0600

    fixed bugs for deployment

--- api/dist/shared/prisma.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient({
-    errorFormat: 'minimal'
-});
-exports.default = prisma;
