9c663a5cf19036e9b0d88041333d01475cf6a11e
ujjalzaman <muzzwal97@gmail.com>
2024-02-25 00:32:59 +0600

    Updated Dashboard Empty Message and Contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -29,13 +29,6 @@ const Contact = () => {
             <Header />
             <SubHeader title="Contact us" subtitle="Lorem ipsum dolor sit amet consectetur adipisicing." />
             <section id="contact" className="contact mt-5 mb-5">
-                <div>
-                    <div className="container">
-                        {/* eslint-disable-next-line */}
-                        <iframe style={{ border: 0, width: "100%", height: "350px" }} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
-                    </div>
-                </div>
-
                 <div className="container" style={{ marginTop: 80, marginBottom: 120 }}>
                     <div className="row">
 
@@ -112,6 +105,12 @@ const Contact = () => {
                                 </form>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="container">
+                        {/* eslint-disable-next-line */}
+                        <iframe style={{ border: 0, width: "100%", height: "350px" }} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
                     </div>
                 </div>
             </section>
