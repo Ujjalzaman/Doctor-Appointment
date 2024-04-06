09078d090beadde398c87b764fdd67c0e94ac0b7
ujjalz <ujjalz@thegates.biz>
2024-04-06 17:56:36 +0600

    init

--- api/src/app/modules/appointment/appointment.interface.ts
+++
@@ -1,2 +0,0 @@
-export const AppointmentsFilterable = ['searchTerm', 'appointmentTime','status']
-export const AppointmentsSearchable = ['appointmentTime','status', 'purpose']
--- api/src/app/modules/contact/contact.route.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { ContactController } from './contact.controller';
-
-const router = express.Router();
-router.post('/', ContactController.ContactUs);
-
-export const ContactRouter = router;
